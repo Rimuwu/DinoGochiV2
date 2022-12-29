51c574b9450c45bfd2b3ab5d9f3abcb997e045e7
AS1 <arseniykofanov@gmail.com>
2022-12-29 20:53:50 +0300

    Скелетик

---
+++ bot/main.py
@@ -0,0 +1,13 @@
+import asyncio
+
+import config
+from telebot import types
+from telebot.async_telebot import AsyncTeleBot
+from telebot.asyncio_handler_backends import State, StatesGroup
+from telebot.asyncio_storage import StateMemoryStorage
+
+bot = AsyncTeleBot(config.BOT_TOKEN, state_storage=StateMemoryStorage())
+
+if __name__ == '__main__':
+    print('start')
+    asyncio.run(bot.infinity_polling())
