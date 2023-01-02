925c80cb1c9774fd2afa1ee24dc46355a5f0a051
magecode <magecode7@yandex.ru>
2023-01-03 01:10:23 +0300

    Антифлуд система

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,5 @@
 from . import config
 from . import exec
+from . import middlewares
 from . import filters
 from . import handlers
---
+++ bot/middlewares/antiflood.py
@@ -0,0 +1,24 @@
+from telebot.asyncio_handler_backends import BaseMiddleware, SkipHandler
+from telebot.types import Message
+from bot.exec import bot
+
+DEFAULT_RATE_LIMIT = 1
+
+class AntifloodMiddleware(BaseMiddleware):
+
+    throttle_dict = {}
+
+    def __init__(self, limit=DEFAULT_RATE_LIMIT):
+        self.last_time = {}
+        self.limit = limit
+        self.update_types = ['message']
+
+    async def pre_process(self, message: Message, data: dict):
+        if not message.from_user.id in self.last_time:
+            self.last_time[message.from_user.id] = message.date
+            return
+        if message.date - self.last_time[message.from_user.id] < self.limit:
+            return SkipHandler()
+        self.last_time[message.from_user.id] = message.date
+
+bot.setup_middleware(AntifloodMiddleware())
