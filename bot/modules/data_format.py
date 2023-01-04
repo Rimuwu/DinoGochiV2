1027df22d59282f886c4f34d67b628c242e2bb45
AS1 <arseniykofanov@gmail.com>
2023-01-04 21:26:49 +0300

    Перенос формата данных в отдельный файл

---
+++ bot/modules/data_format.py
@@ -0,0 +1,37 @@
+import random
+
+class DataFormat:
+
+    def __init__(self) -> None:
+        '''Функции форматизации данных'''
+    
+    def random_dict(data: dict) -> dict:
+        """ Предоставляет общий формат данных, подерживающий 
+            случайные и статичные числа.
+        
+        Примеры словаря:
+          >>> {"min": 1, "max": 2, "type": "random"}
+          >>> {"act": 1, "type": "static"}
+        """
+
+        if 'type' in data.keys():
+            if data["type"] in ["static", "random"]:
+
+                if data["type"] == "static":
+                    return data['act']
+
+                elif data["type"] == "random":
+                    if data['min'] >= data['max']:
+                        return 0
+                    else:
+                        return random.randint(data['min'], data['max'])
+                else:
+                    return 0
+            else:
+                return data
+        else:
+            return data
+
+
+if __name__ == '__main__':
+    raise Exception("This file cannot be launched on its own!")
