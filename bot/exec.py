2d28ceddf3bd1a0b7368186ece7665dccd03de26
AS1 <arseniykofanov@gmail.com>
2023-01-04 21:25:59 +0300

    Перенос логов в отдельный файл

--- bot/exec.py
+++ bot/exec.py
@@ -8,11 +8,12 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_handler_backends import State, StatesGroup
 from telebot.asyncio_storage import StateMemoryStorage
 
-from bot.modules.functions import LogFuncs
+from bot.modules.logs import LogFuncs
 
 bot = AsyncTeleBot(config.conf.bot_token, state_storage=StateMemoryStorage())
 log = LogFuncs()
 
 def run():
+    log.create_log()
     log.console_message('start')
     asyncio.run(bot.infinity_polling())
