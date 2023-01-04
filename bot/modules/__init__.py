ed056a313a7fa7e31508eaa82fe92c9fbd369ee5
AS1 <arseniykofanov@gmail.com>
2023-01-04 21:26:30 +0300

    Импорты

---
+++ bot/modules/__init__.py
@@ -0,0 +1,7 @@
+import bot.modules.logs
+import bot.modules.localization
+import bot.modules.data_format
+import bot.modules.item
+import bot.modules.dinosaur
+import bot.modules.user
+import bot.modules.dungeon
