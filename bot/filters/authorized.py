886753ce1d13288f9cd9f80e2bb1af9ae99d2c66
AS1 <arseniykofanov@gmail.com>
2023-01-06 15:00:06 +0300

    Уьраны ненужные импорты

--- bot/filters/authorized.py
+++ bot/filters/authorized.py
@@ -1,12 +1,11 @@
 from telebot.asyncio_filters import AdvancedCustomFilter
-from telebot.types import Message
 from bot.exec import bot
-from bot.config import conf, mongo_client
+from bot.config import  mongo_client
 
 users = mongo_client.bot.users
 
 
-class IsAuthorizedUser(telebot.asyncio_filters.AdvancedCustomFilter):
+class IsAuthorizedUser(AdvancedCustomFilter):
     key = 'is_authorized'
 
     async def check(self, message, status:bool):
