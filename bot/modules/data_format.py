06ecdfa9075fc174a563bd6c380281d5dc82cbf2
AS1 <arseniykofanov@gmail.com>
2023-01-07 18:28:21 +0300

    Убран класс.

--- bot/modules/data_format.py
+++ bot/modules/data_format.py
@@ -1,36 +1,32 @@
 import random
 
-class DataFormat:
-
-    def __init__(self) -> None:
-        '''Функции форматизации данных'''
     
-    def random_dict(data: dict) -> dict:
-        """ Предоставляет общий формат данных, подерживающий 
-            случайные и статичные числа.
-        
-        Примеры словаря:
-          >>> {"min": 1, "max": 2, "type": "random"}
-          >>> {"act": 1, "type": "static"}
-        """
+def random_dict(data: dict) -> dict:
+    """ Предоставляет общий формат данных, подерживающий 
+        случайные и статичные числа.
+    
+    Примеры словаря:
+        >>> {"min": 1, "max": 2, "type": "random"}
+        >>> {"act": 1, "type": "static"}
+    """
 
-        if 'type' in data.keys():
-            if data["type"] in ["static", "random"]:
+    if 'type' in data.keys():
+        if data["type"] in ["static", "random"]:
 
-                if data["type"] == "static":
-                    return data['act']
+            if data["type"] == "static":
+                return data['act']
 
-                elif data["type"] == "random":
-                    if data['min'] >= data['max']:
-                        return 0
-                    else:
-                        return random.randint(data['min'], data['max'])
-                else:
+            elif data["type"] == "random":
+                if data['min'] >= data['max']:
                     return 0
+                else:
+                    return random.randint(data['min'], data['max'])
             else:
-                return data
+                return 0
         else:
             return data
+    else:
+        return data
 
 
 if __name__ == '__main__':
