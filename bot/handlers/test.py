29667819e403078e79a4f3863e283dc4225003f7
AS1 <arseniykofanov@gmail.com>
2023-01-07 18:28:10 +0300

    Тестовые команды иллюстрирующие работу некоторых функций.

--- bot/handlers/test.py
+++ bot/handlers/test.py
@@ -1,10 +1,10 @@
 # Тестовые команды
 
-from bot import config
-
 from telebot import types
 from bot.exec import bot
 from bot.modules.localization import t, get_all_locales
+
+from bot.modules.user import User
 
 
 @bot.message_handler(text="buttons_name.back")
@@ -12,11 +12,22 @@ async def back_command(message: types.Message):
     await bot.send_message(message.chat.id, t("language_name", message.from_user.language_code))
 
 
-@bot.message_handler(content_types=['text'], func=lambda message: message.text == '+')
+@bot.message_handler(content_types=['text'], func=lambda message: message.text == 'local_test')
+async def on_message(message):
+    loc = t('start_game', 'ru')
+    print(loc)
+    
+
+@bot.message_handler(content_types=['text'], func=lambda message: message.text == 'user_data')
 async def on_message(message):
     user = message.from_user
-    locales = get_all_locales('language_name')
 
-    for key in locales:
-        await bot.send_message(message.chat.id, t("language_name", key))
+    dinos = User(user.id).get_dinos()
+    inv = User(user.id).get_inventory()
+
+    for i in dinos:
+        print(i.__dict__)
+    
+    print(inv)
+
 
