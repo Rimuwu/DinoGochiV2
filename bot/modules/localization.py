c56ea1473815386eb7690b0106506886b1a03915
AS1 <arseniykofanov@gmail.com>
2023-01-07 15:43:10 +0300

    Все настрйоки локализации перенесены в функцию. Добавлена настройка доступных локализаций.

--- bot/modules/localization.py
+++ bot/modules/localization.py
@@ -1,13 +1,27 @@
 # Модуль загрузки локлизации
 
-from i18n import resource_loader
-from i18n import translator
-from i18n import config
+import json
+import os
 
-config.set("file_format", "json")
-config.set("load_path", ["./bot/localization"])
-config.set("filename_format", "{locale}.{format}")
-resource_loader.init_json_loader()
+from i18n import config, resource_loader, translator
+
+
+def load() -> None:
+    """Настройки локализации
+    """
+    config.set("file_format", "json")
+    config.set("load_path", ["./bot/localization"])
+    config.set("filename_format", "{locale}.{format}")
+    resource_loader.init_json_loader()
+
+    lang_list = []
+    for filename in os.listdir("./bot/localization"):
+        with open(f'./bot/localization/{filename}', encoding='utf-8') as f:
+            langs_dict = json.load(f)
+            for lang_code in langs_dict.keys():
+                lang_list.append(lang_code)
+
+    config.set('available_locales', lang_list)
 
 def t(key: str, locale: str = "en", **kwargs) -> str:
     """Возвращает текст на нужном языке
@@ -37,8 +51,6 @@ def get_all_locales(key:str, **kwargs):
     lang_keys = config.get("available_locales")
     locales_dict = {}
 
-    print(lang_keys)
-
     for locale in lang_keys:
         try:
             locales_dict[locale] = translator.t(key, locale=locale, **kwargs)
@@ -47,6 +59,7 @@ def get_all_locales(key:str, **kwargs):
     
     return locales_dict
 
-
 if __name__ == '__main__':
     raise Exception("This file cannot be launched on its own!")
+else:
+    load()
