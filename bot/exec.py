8e0b8cc0d6ceeb5963930d3d747e09de8922d537
AS1 <arseniykofanov@gmail.com>
2023-01-19 19:12:51 +0300

    Переименована функция.

--- bot/exec.py
+++ bot/exec.py
@@ -7,12 +7,12 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_handler_backends import State, StatesGroup
 from telebot.asyncio_storage import StateMemoryStorage
 
-from bot.modules.logs import console_message, create_log
+from bot.modules.logs import log, create_log
 from bot.config import conf
 
 bot = AsyncTeleBot(conf.bot_token, state_storage=StateMemoryStorage())
 
 def run():
     create_log()
-    console_message('start')
+    log('start')
     asyncio.run(bot.infinity_polling())
