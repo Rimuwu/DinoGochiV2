68394c3f7a0ff84149fe27599ca8aafcb8d379cf
AS1 <arseniykofanov@gmail.com>
2023-01-19 19:15:09 +0300

    Команда для теста ссылок локализации.

--- bot/handlers/test.py
+++ bot/handlers/test.py
@@ -14,13 +14,12 @@ async def back_command(message: Message):
 
 @bot.message_handler(content_types=['text'], func=lambda message: message.text == 'local_test')
 async def local_test_command(message: Message):
-    loc = t('start_game', 'ru')
-    print(loc)
+    loc = t('test_key_link', 'ru')
+    await bot.send_message(message.chat.id, loc)
     
 
 @bot.message_handler(content_types=['text'], func=lambda message: message.text == 'user_data')
 async def user_data_command(message: Message):
     user = User(message.from_user.id)
-
     user.view()
 
