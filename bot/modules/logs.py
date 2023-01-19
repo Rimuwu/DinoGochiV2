762c9135f128d3bf5792f5572945c9c283c64277
AS1 <arseniykofanov@gmail.com>
2023-01-19 19:26:20 +0300

    Мелкие изменения, красотульки. Уровень дебага для лога и префикс для сообщения.

--- bot/modules/logs.py
+++ bot/modules/logs.py
@@ -1,38 +1,42 @@
 import logging
-import time
+from time import strftime
 
 from colorama import Fore, Style
 
 from bot.config import conf
 
 
-def console_message(message, lvl = 1) -> None:
+def log(message: str, lvl: int = 1, prefix: str = 'Бот') -> None:
     """
     LVL: \n
+    0 - debug
     1 - info\n
     2 - warning\n
     3 - error\n
     4 - critical
     """
 
-    if lvl == 1:
+    if lvl == 0:
+        logging.info(f'DEBUG: {message}')
+        print(Fore.CYAN + f"{strftime('%Y %m-%d %H.%M.%S')} {prefix}: {message}" + Style.RESET_ALL)
+    elif lvl == 1:
         logging.info(message)
-        print(Fore.GREEN + f"{time.strftime('%Y %m-%d %H.%M.%S')} Бот: {message}" + Style.RESET_ALL)
+        print(Fore.GREEN + f"{strftime('%Y %m-%d %H.%M.%S')} {prefix}: {message}" + Style.RESET_ALL)
     elif lvl == 2:
         logging.warning(message)
-        print(Fore.BLUE + f"{time.strftime('%Y %m-%d %H.%M.%S')} Бот: {message}" + Style.RESET_ALL)
+        print(Fore.BLUE + f"{strftime('%Y %m-%d %H.%M.%S')} {prefix}: {message}" + Style.RESET_ALL)
     elif lvl == 3:
         logging.error(message)
-        print(Fore.YELLOW + f"{time.strftime('%Y %m-%d %H.%M.%S')} Бот: {message}" + Style.RESET_ALL)
+        print(Fore.YELLOW + f"{strftime('%Y %m-%d %H.%M.%S')} {prefix}: {message}" + Style.RESET_ALL)
     else:
         logging.critical(message)
-        print(Fore.RED + f"{time.strftime('%Y %m-%d %H.%M.%S')} Бот: {message}" + Style.RESET_ALL)
+        print(Fore.RED + f"{strftime('%Y %m-%d %H.%M.%S')} {prefix}: {message}" + Style.RESET_ALL)
 
 def create_log():
 
     logging.basicConfig(
         level=logging.INFO,
-        filename=f"{conf.logs_dir}/{time.strftime('%Y %m-%d %H.%M.%S')}.log",
+        filename=f"{conf.logs_dir}/{strftime('%Y %m-%d %H.%M.%S')}.log",
         filemode="w", encoding='utf-8',
         format="%(asctime)s %(levelname)s %(message)s"
     )
