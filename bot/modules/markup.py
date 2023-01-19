53747f137e54990355c6d9a420a1cfccfd3ebad4
AS1 <arseniykofanov@gmail.com>
2023-01-19 19:26:38 +0300

    Функция для маркапа, подготовка

---
+++ bot/modules/markup.py
@@ -0,0 +1,7 @@
+from telebot.types import ReplyKeyboardMarkup, User
+
+def markups_menu(markup_key: str = 'main_menu', language_code: str = 'en'):
+    """Главная функция создания меню для клавиатур
+    """
+
+    ...
