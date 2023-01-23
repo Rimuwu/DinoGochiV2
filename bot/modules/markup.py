93e1d91f020a3ef721e7f92d2c59db8c1731e57d
AS1 <arseniykofanov@gmail.com>
2023-01-23 16:37:14 +0300

    Создана логика функции меню

--- bot/modules/markup.py
+++ bot/modules/markup.py
@@ -1,7 +1,33 @@
 from telebot.types import ReplyKeyboardMarkup, User
+from bot.modules.localization import tranlate_data
+from bot.modules.data_format import list_to_keyboard
 
-def markups_menu(markup_key: str = 'main_menu', language_code: str = 'en'):
+def markups_menu(markup_key: str = 'main_menu', language_code: str = 'en', **kwargs):
     """Главная функция создания меню для клавиатур
     """
+    prefix = ''
+    buttons = []
 
-    ...
+    if markup_key == 'main_menu':
+        """ Главное меню
+
+            Дополнительые аргументы:
+            faq - если True, то кнопка справочника не показывается
+        """
+        prefix = 'commands_name.'
+        buttons = [
+            ['dino_profile', 'actions_menu', 'profile_menu'],
+            ['settings_menu', 'friends_menu', 'faq'],
+            ['dino-tavern_menu']
+        ]
+
+        if kwargs.get('faq', False): #Если передаём faq, то можно удалить кнопку
+            buttons[1].remove('faq')
+    
+    
+    buttons = tranlate_data(
+        data=buttons, 
+        locale=language_code, 
+        key_prefix=prefix) #Переводим текст внутри списка
+
+    return list_to_keyboard(buttons) # type: ignore    
