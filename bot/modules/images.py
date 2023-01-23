8c636598c6e6a41adda43b8ce6c8fb521050f6b9
AS1 <arseniykofanov@gmail.com>
2023-01-23 16:36:49 +0300

    Документация

--- bot/modules/images.py
+++ bot/modules/images.py
@@ -4,14 +4,21 @@ from bot.config import conf
 from bot.const import GAME_SETTINGS, DINOS
 
 def trans_paste(fg_img, bg_img, alpha=10.0, box=(0, 0)):
+    """Накладывает одно изображение на другое.
+    """
     fg_img_trans = Image.new("RGBA", fg_img.size)
     fg_img_trans = Image.blend(fg_img_trans, fg_img, alpha)
     bg_img.paste(fg_img_trans, box, fg_img_trans)
+
     return bg_img
 
 def create_eggs_image():
+    """Создаёт изображение выбора яиц.
+    """
     id_l = []
-    bg_p = Image.open(f"images/remain/backs/{choice(GAME_SETTINGS['egg_ask_backs'])}.png")
+    bg_p = Image.open(
+        f"images/remain/backs/{choice(GAME_SETTINGS['egg_ask_backs'])}.png"
+        )
 
     for i in range(3):
         rid = str(choice(list(DINOS['data']['egg'])))
