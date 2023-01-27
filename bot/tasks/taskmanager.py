1ba5f55ee2ed54b62143c3ef3a3a51665a77e4b5
AS1 <arseniykofanov@gmail.com>
2023-01-27 21:00:33 +0300

    Система тасков

--- bot/tasks/taskmanager.py
+++ bot/tasks/taskmanager.py
@@ -1,7 +1,39 @@
+import threading
+import asyncio
+from bot.modules.logs import log
 
-async def add_tack():
-    ...
+task_dict = {}
 
+def add_task(function, time_type: str='second', units: int=1, wait: int=0):
+    """Добавить для функции таймер выполнения 
+        time_type: str - second / minute / hour
+        units: int - повторение каждые ? секунд / минут / часов
+    """
 
-async def add_task():
-    ...
+    if time_type == 'minute':
+        seconds = units * 60
+    elif time_type == 'hour':
+        seconds = units * 3600
+    else:
+        seconds = units
+    
+    if function in task_dict:
+        raise Exception(f'Функция {function.__name__} добавлена повторно.')
+        
+    task_dict[function] = seconds, wait
+    return True
+
+async def task_executor(function, seconds: int, wait: int=0):
+    """Исполнитель всех задач с обработчиком ошибок и созданием потока
+       seconds: int - количество секунд между повторами
+       wait: int - ожидание перед стартом
+    """
+    
+    await asyncio.sleep(wait)
+    while True:
+        try:
+            threading.Thread(target=asyncio.run, args=(function(),)).start()
+        except Exception as error:
+            log(prefix=F"{function.__name__} error", message=str(error), lvl=3)
+        
+        await asyncio.sleep(seconds)
