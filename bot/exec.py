9157120ed778fe4ab3f878419aeeb619e64c5d0e
AS1 <arseniykofanov@gmail.com>
2023-01-27 20:59:36 +0300

    Прикольчик для разнообразия и подключение тасков.

--- bot/exec.py
+++ bot/exec.py
@@ -7,14 +7,15 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.asyncio_handler_backends import State, StatesGroup
 from telebot.asyncio_storage import StateMemoryStorage
 
-from bot.modules.logs import log, create_log
 from bot.config import conf
+from bot.modules.logs import create_log, log
+from bot.tasks.taskmanager import task_dict, task_executor
 
 bot = AsyncTeleBot(conf.bot_token, state_storage=StateMemoryStorage())
 
 def run():
     create_log()
-    log('start')
+    log('Привет! Я вижу ты так и не починил тот самый баг на 46-ой строчке...')
 
     # заготовка поддержки асинхронных потоков
     ioloop = asyncio.get_event_loop()
@@ -22,6 +23,13 @@ def run():
     tasks = [
         ioloop.create_task(bot.infinity_polling())
     ]
+
+    for func, data in task_dict.items():
+        tasks.append(
+            ioloop.create_task(
+                task_executor(function=func, seconds=data[0], wait=data[1])
+            )
+        )
 
     wait_tasks = asyncio.wait(tasks)
 
