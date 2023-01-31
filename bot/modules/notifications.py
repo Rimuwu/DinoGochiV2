553a9e98df6ee95f73d19358efa38b94f551b0cb
AS1 <arseniykofanov@gmail.com>
2023-01-31 16:18:28 +0300

    Скелет уведомлений

---
+++ bot/modules/notifications.py
@@ -0,0 +1,7 @@
+# from bot.exec import bot
+
+def dino_notification(dino_id, not_type: str):
+    print(dino_id, not_type)
+
+def user_notification(user_id: int, not_type: str):
+    print(user_id, not_type)
