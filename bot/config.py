3b7bf9031a9a9560eb4896f078622125ba7ccec1
AS1 <arseniykofanov@gmail.com>
2023-01-31 16:05:08 +0300

    Убраны не используемые переменные, добавлена настройка активации побочных заданий. Добавлена проверка базы.

--- bot/config.py
+++ bot/config.py
@@ -16,11 +16,10 @@ class Config:
         """Класс настроек бота. Все основные переменные хранятся здесь
         """
         self.bot_token = 'NOTOKEN'
-        self.bot_name = 'NONAME'
         self.bot_devs = []
         self.temp_dir = 'bot/temp'
         self.logs_dir = 'bot/logs'
-        self.is_ignore_name = True
+        self.active_tasks = True
         self.bot_group_id = 0
         self.mongo_url = 'mongodb://localhost:27017'
         self.debug = False
@@ -43,12 +42,31 @@ class Config:
             sort_keys=True, indent=4)
  
 conf = Config()
+mongo_client = pymongo.MongoClient(conf.mongo_url)
+
 for way in conf.temp_dir, conf.logs_dir:
     if not os.path.exists(way):
         os.mkdir(way) #Создаёт папку в директории  
         print(f"I didn't find the {way} directory, so I created it.")
 
+def check_base():
+    created_base = mongo_client.list_database_names()
+    needful_base = ['bot', 'tasks']
+    collections = {
+        'bot': ['users', 'dinosaurs', 'items', 'friends', 'quests', 'products', 'referals', 'management'],
+        'tasks': ['game', 'incubation', 'journey', 'sleep']
+    }
+
+    for base in needful_base:
+        if base not in created_base:
+            database = mongo_client[base]
+            for col in collections[base]:
+                database.create_collection(col)
+    
+    print('The databases are checked and prepared for use.')
+
 if __name__ == '__main__':
+    check_base() # Проверка базы данных на наличие коллекций
     with open(CONFIG_PATH, 'w') as f:
         f.write(conf.toJSON())
         sys.exit(f"{CONFIG_PATH} created! Please don't forget to set it up!")
@@ -58,5 +76,3 @@ else:
             conf.fromJSON(f.read())
     else:
         sys.exit(f"{CONFIG_PATH} missed! Please, run {__name__}")
-
-mongo_client = pymongo.MongoClient(conf.mongo_url)
