f5ac474120000f12e88403aa226d6467ed383abe
AS1 <arseniykofanov@gmail.com>
2023-01-31 16:19:14 +0300

    task проверки инкубируемых динозавров

--- bot/tasks/incubation.py
+++ bot/tasks/incubation.py
@@ -1,13 +1,22 @@
 from bot.tasks.taskmanager import add_task
-
-async def incuation():
-    print('incubation')
-
-async def inc():
-    print('inc')
+from bot.config import mongo_client, conf
+from time import time
+from bot.modules.notifications import user_notification
+from bot.modules.dinosaur import insert_dino
 
 
+incubations = mongo_client.tasks.incubation
 
+async def incubation():
+    """Проверка инкубируемых яиц
+    """
+    data = list(incubations.find({'incubation_time': {'$lte': time()}})).copy()
+
+    for egg in data:
+        insert_dino(egg['owner_id'], egg['dino_id']) #создаём динозавра
+        incubations.delete_one({'_id': egg['_id']}) #удаляем динозавра из инкубаций
+        user_notification(egg['owner_id'], 'incubation_ready') #отправляем уведомления
+    
 if __name__ != '__main__':
-    add_task(incuation, 1, 3)
-    add_task(inc, 5)
+    if conf.active_tasks:
+        add_task(incubation, 10.0, 1.0)
