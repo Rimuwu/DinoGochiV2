4b1e9153cc8a5675db2063a58a742d5c28a2e6ec
AS1 <arseniykofanov@gmail.com>
2023-02-01 19:51:53 +0300

    Мультиязычность для фильтра

--- bot/filters/translated_text.py
+++ bot/filters/translated_text.py
@@ -3,15 +3,21 @@
 from telebot.asyncio_filters import AdvancedCustomFilter
 from telebot.types import Message
 from bot.exec import bot
-from bot.modules.localization import t
+from bot.modules.localization import get_all_locales
 
+lang_commands = get_all_locales('commands_name') # {'en': {'start_game': ...}, }
+commands = {} # {'key': [...], }
+for lang in lang_commands:
+    for command_key in lang_commands[lang]:
+        commands[command_key] = commands.get(command_key, [])
+        commands[command_key].append(lang_commands[lang][command_key])
 
 class IsEqual(AdvancedCustomFilter):
     key = 'text'
 
     async def check(self, message: Message, key: str):
-        text = t(key, message.from_user.language_code)
-        return text == message.text
+        if commands.get(key, 0):
+            return message.text in commands[key]
 
 
 bot.add_custom_filter(IsEqual())
