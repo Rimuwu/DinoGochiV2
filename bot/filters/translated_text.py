12b3842abdb4321edbefb28da5d25c092dccc189
AS1 <arseniykofanov@gmail.com>
2023-02-02 17:53:11 +0300

    Меня не ценят как слизь, так что у всех теперь не будет выбора

--- bot/filters/translated_text.py
+++ bot/filters/translated_text.py
@@ -3,23 +3,14 @@
 from telebot.asyncio_filters import AdvancedCustomFilter
 from telebot.types import Message
 from bot.exec import bot
-from bot.modules.localization import get_all_locales
-
-commands = {} # {'key.key': [...], }
-directirys = ['commands_name', 'buttons_name'] # {'en': {'start_game': ...}, }
-for direct in directirys:
-    lang_commands = get_all_locales(direct)
-    for lang in lang_commands:
-        for command_key in lang_commands[lang]:
-            commands[f'{direct}.{command_key}'] = commands.get(f'{direct}.{command_key}', [])
-            commands[f'{direct}.{command_key}'].append(lang_commands[lang][command_key])
+from bot.modules.localization import t
 
 class IsEqual(AdvancedCustomFilter):
     key = 'text'
 
     async def check(self, message: Message, key: str):
-        if commands.get(key, 0):
-            return message.text in commands[key]
+        text = t(key, message.from_user.language_code)
+        return text == message.text
 
 
 bot.add_custom_filter(IsEqual())
