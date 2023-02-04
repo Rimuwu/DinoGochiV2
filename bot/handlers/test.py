c097013467d079f8b5e1ba38d2feb668a1bb9807
magecode <magecode7@yandex.ru>
2023-02-04 18:28:11 +0300

    Оптимизация
    О, яички

--- bot/handlers/test.py
+++ bot/handlers/test.py
@@ -2,9 +2,8 @@
 
 from telebot.types import Message
 from bot.exec import bot
-from bot.modules.notifications import user_notification
+from bot.handlers.start import start_game
 
 @bot.message_handler(commands=['test'])
 async def test_command(message: Message):
-    await user_notification(message.from_user.id, 'test')
-
+    await start_game(message)
