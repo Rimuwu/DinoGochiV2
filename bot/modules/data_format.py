7eb1322d1edcabb0a29a22f2dd16a01f2ecc0d14
AS1 <arseniykofanov@gmail.com>
2023-02-05 17:55:48 +0300

    Функция рандома редкости динозавра перенесена в dinosaur. Функция для перевода секунд в строку

--- bot/modules/data_format.py
+++ bot/modules/data_format.py
@@ -1,6 +1,9 @@
 import random
-from ..const import GAME_SETTINGS
+
 from telebot.types import ReplyKeyboardMarkup, User
+
+from bot.modules.localization import get_data
+
 
 def chunks(lst: list, n: int):
     for i in range(0, len(lst), n):
@@ -74,15 +77,6 @@ def user_name(user: User):
         else:
             return user.first_name
 
-def random_quality() -> str:
-    """Случайная редкость
-    """
-    rarities = list(GAME_SETTINGS['dino_rarity'].keys())
-    weights = list(GAME_SETTINGS['dino_rarity'].values())
-
-    quality = random.choices(rarities, weights)[0]
-    return quality
-
 def random_code(col: int=10):
     """Генерирует случайный код из букв и цыфр
     """
@@ -101,6 +95,68 @@ def random_code(col: int=10):
             code += str(random.randint(0, 9))
 
     return code
+
+def seconds_to_str(seconds: int, lang: str='en', mini: bool=False):
+    """Преобразует число секунд в строку
+       Example:
+       > seconds=10000 lang='ru'
+       > 1 день 2 минуты 41 секунда
+       
+       > seconds=10000 lang='ru' mini=True
+       > 1 д. 2 мин. 41 сек.
+    """
+    time_format = get_data('time_format', lang) # type: dict
+    result = ''
+
+    def ending_w(time_type: str, unit: int) -> str:
+        if mini:
+            return time_format[time_type][3]
+        
+        else:
+            result = ''
+            if int(unit) not in [11, 12, 13, 14, 15]:
+                number = int(str(unit)[int(len(str(unit))) - 1:])
+            else:
+                number = int(unit)
+                
+            if number == 1:
+                result = time_format[time_type][0]
+            elif number in [2, 3, 4]:
+                result = time_format[time_type][1]
+            elif number > 4 or number == 0:
+                result = time_format[time_type][2]
+        
+        return result
+    
+    def seconds_to_time(seconds: int):
+        time_calculation = {
+            'day': 86400, 'hour': 3600, 
+            'minute': 60, 'second': 1
+        }
+        time_dict = {
+            'day': 0, 'hour': 0, 
+            'minute': 0, 'second': 0
+        }
+
+        for tp, unit in time_calculation.items():
+            tt = seconds // unit
+
+            if tt:
+                seconds -= tt * unit
+                time_dict[tp] = tt
+        
+        return time_dict 
+
+    data = seconds_to_time(seconds=seconds)
+    for tp, unit in data.items():
+        if unit:
+            if mini:
+                result += f'{unit}{ending_w(tp, unit)} '
+            else:
+                result += f'{unit} {ending_w(tp, unit)} '
+    
+    return result
+
 
 
 if __name__ == '__main__':
