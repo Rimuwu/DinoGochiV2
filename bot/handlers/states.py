d4356094b48caedb4b6043311d5ae1439818b838
AS1 <arseniykofanov@gmail.com>
2023-02-06 17:51:23 +0300

    Возврат в последнее меню

--- bot/handlers/states.py
+++ bot/handlers/states.py
@@ -1,6 +1,8 @@
 from telebot.asyncio_handler_backends import State, StatesGroup
 from telebot.types import Message
+
 from bot.exec import bot
+from bot.modules.markup import markups_menu as m
 
 
 @bot.message_handler(text='buttons_name.cancel', state='*')
@@ -9,7 +11,9 @@ async def cancel(message: Message):
     Состояние отмены
     """
 
-    await bot.send_message(message.chat.id, "❌")
+    await bot.send_message(message.chat.id, "❌", 
+                           reply_markup=m(message.from_user.id, 
+                           'last_menu', message.from_user.language_code))
     await bot.delete_state(message.from_user.id, message.chat.id)
     await bot.reset_data(message.from_user.id,  message.chat.id)
 
