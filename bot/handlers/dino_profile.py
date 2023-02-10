c02ccfca6ec261bacfbb6116d186ecc39a8feb7d
AS1 <arseniykofanov@gmail.com>
2023-02-10 17:11:15 +0300

    -

--- bot/handlers/dino_profile.py
+++ bot/handlers/dino_profile.py
@@ -3,8 +3,8 @@ from telebot import types
 from bot.exec import bot
 from bot.modules.data_format import chunks, list_to_keyboard, near_key
 from bot.modules.dinosaur import Dino, Egg
-from bot.modules.localization import t, get_data
-from bot.modules.markup import markups_menu as m
+from bot.modules.localization import t
+from bot.modules.markup import get_answer_keyboard, markups_menu as m
 from bot.modules.states import States
 from bot.modules.user import User
 from bot.modules.events import get_one_event
@@ -38,38 +38,22 @@ async def dino_profile(message: types.Message):
     lang = message.from_user.language_code
     elements = user.get_dinos() + user.get_eggs()
 
-    if len(elements) == 0:
+    data = get_answer_keyboard(elements, lang)
+
+    if data['case'] == 0:
         await bot.send_message(user.userid, 
             t('p_profile.no_dinos_eggs'), lang)
 
-    elif len(elements) == 1:
-        element = elements[0]
-        await send_m(user.userid, element, lang)
+    elif data['case'] == 1:
+        await send_m(user.userid, data['element'], lang)
 
     else: # Несколько динозавров / яиц
-        names, data_names = [], {}
-        n, txt = 0, ''
-        for element in elements:
-            n += 1
-
-            if type(element) == Dino:
-                txt = f'{n}🦕 {element.name}'
-            elif type(element) == Egg:
-                txt = f'{n}🥚'
-            
-            data_names[txt] = element
-            names.append(txt)
-            
-        buttons_list = list(chunks(names, 2))
-        buttons_list.append([t('buttons_name.cancel', lang)])
-        keyboard = list_to_keyboard(buttons_list, 2)
-
         # Устанавливаем состояния и передаём данные
         await bot.set_state(user.userid, States.choose_dino, message.chat.id)
         async with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
-            data['dino_answer'] = data_names
+            data['dino_answer'] = data['data_names']
 
-        await bot.send_message(user.userid, t('p_profile.choose_dino', lang), reply_markup=keyboard)
+        await bot.send_message(user.userid, t('p_profile.choose_dino', lang), reply_markup=data['keyboard'])
 
 @bot.message_handler(state=States.choose_dino, is_authorized=True)
 async def answer_dino(message: types.Message):
