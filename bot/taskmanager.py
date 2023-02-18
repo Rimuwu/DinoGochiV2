44d3aef9f9d78cf5d1017327a433240f5549ea9c
AS1 <arseniykofanov@gmail.com>
2023-02-18 13:12:27 +0300

    Возможность передачи аргументов в таски

--- bot/taskmanager.py
+++ bot/taskmanager.py
@@ -4,7 +4,7 @@ from bot.modules.logs import log
 ioloop = asyncio.get_event_loop()
 tasks = []
 
-async def _task_executor(function, repeat_time: float, delay: float):
+async def _task_executor(function, repeat_time: float, delay: float, **kwargs):
     """Исполнитель всех задач с обработчиком ошибок и созданием потока
     """
     await asyncio.sleep(delay)
@@ -12,19 +12,19 @@ async def _task_executor(function, repeat_time: float, delay: float):
     if repeat_time:
         while True:
             try:
-                await function()
+                await function(**kwargs)
             except Exception as error:
                 log(prefix=F"{function.__name__} error", message=str(error), lvl=3)
             
             await asyncio.sleep(repeat_time)
     else:
         try:
-            await function()
+            await function(**kwargs)
         except Exception as error:
             log(prefix=F"{function.__name__} error", message=str(error), lvl=3)
             
 
-def add_task(function, repeat_time: float=0, delay: float=0):
+def add_task(function, repeat_time: float=0, delay: float=0, **kwargs):
     """Добавить задачу в асинхрон
 
     Args:
@@ -32,8 +32,8 @@ def add_task(function, repeat_time: float=0, delay: float=0):
         repeat_time (int, optional): время повтора, если 0 то задача не зациклена. Defaults to 0.
         delay (int, optional): задержка. Defaults to 0.
     """
-    
-    task = ioloop.create_task(_task_executor(function, repeat_time, delay))
+
+    task = ioloop.create_task(_task_executor(function, repeat_time, delay, **kwargs))
 
     assert task not in tasks, f'Функция {function.__name__} добавлена повторно.'
     
