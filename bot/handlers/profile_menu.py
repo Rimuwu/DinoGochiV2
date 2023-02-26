a24b8bc1e11068c8afd0dd0a090ba63f046409b9
AS1 <arseniykofanov@gmail.com>
2023-02-26 20:57:11 +0300

    Заготовка под команды меню

---
+++ bot/handlers/profile_menu.py
@@ -0,0 +1,8 @@
+from telebot.types import Message
+
+from bot.config import mongo_client
+from bot.exec import bot
+from bot.modules.localization import t
+from bot.modules.markup import markups_menu as m
+
+users = mongo_client.bot.users
