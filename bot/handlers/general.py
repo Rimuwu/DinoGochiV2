aff02ff74484b8527c52f7d9e87cb9dcd9bd2782
AS1 <arseniykofanov@gmail.com>
2023-02-27 20:15:05 +0300

    Хендлеры для перехода между панелями

--- bot/handlers/general.py
+++ bot/handlers/general.py
@@ -14,7 +14,8 @@ async def back_buttom(message: Message):
     back_m = back_menu(userid)
     text = t(f'back_text.{back_m}', lang)
 
-    await bot.send_message(message.chat.id, text, reply_markup=m(userid, back_m, lang) )
+    await bot.send_message(message.chat.id, text, 
+                           reply_markup=m(userid, back_m, lang) )
 
 @bot.message_handler(text='commands_name.settings_menu', is_authorized=True)
 async def settings_menu(message: Message):
@@ -24,15 +25,33 @@ async def settings_menu(message: Message):
     user = users.find_one({'userid': userid})
     if user:
         settings = user['settings']
-        text = t("open_settings", lang, 
+        text = t("menu_text.settings", lang, 
                 notif=settings["notifications"], vis_faq=settings['faq'])
         text = text.replace("True", '✅').replace("False", '❌')
 
-        await bot.send_message(message.chat.id, text, reply_markup=m(userid, 'settings_menu', lang))
+        await bot.send_message(message.chat.id, text, 
+                               reply_markup=m(userid, 'settings_menu', lang))
 
 @bot.message_handler(text='commands_name.profile_menu', is_authorized=True)
 async def profile_menu(message: Message):
     userid = message.from_user.id
     lang = message.from_user.language_code
 
-    await bot.send_message(message.chat.id, t('open_profile_menu', lang), reply_markup=m(userid, 'profile_menu', lang))
+    await bot.send_message(message.chat.id, t('menu_text.profile', lang), 
+                           reply_markup=m(userid, 'profile_menu', lang))
+    
+@bot.message_handler(text='commands_name.friends_menu', is_authorized=True)
+async def friends_menu(message: Message):
+    userid = message.from_user.id
+    lang = message.from_user.language_code
+
+    await bot.send_message(message.chat.id, t('menu_text.friends', lang), 
+                           reply_markup=m(userid, 'friends_menu', lang))
+
+@bot.message_handler(text='commands_name.profile.market', is_authorized=True)
+async def market_menu(message: Message):
+    userid = message.from_user.id
+    lang = message.from_user.language_code
+
+    await bot.send_message(message.chat.id, t('menu_text.market', lang), 
+                           reply_markup=m(userid, 'market_menu', lang))
