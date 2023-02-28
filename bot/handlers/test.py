d8db6e0d6c74abcdfe38f1bc6e30beff6d0c1d09
AS1 <arseniykofanov@gmail.com>
2023-02-28 19:01:36 +0300

    -

--- bot/handlers/test.py
+++ bot/handlers/test.py
@@ -2,8 +2,24 @@
 
 from telebot.types import Message
 from bot.exec import bot
-from bot.modules.data_format import near_key_number
+from bot.modules.item import get_name, get_item_dict, get_data
+from bot.config import mongo_client
+from bot.modules.user import User
+
+dinosaurs = mongo_client.bot.dinosaurs
 
 @bot.message_handler(commands=['test'])
 async def test_command(message: Message):
-    await bot.send_message(message.from_user.id, near_key_number(n=0, data={'10': 'много', '5': 'средне', '2': 'мало'}))
+    user_id = message.from_user.id
+
+    user = User(user_id)
+    print(user.get_friends())
+
+@bot.message_handler(start_with='commands_name.actions.dino_button')
+async def test_command2(message: Message):
+    user_id = message.from_user.id
+    print('ok')
+
+
+
+    
