19dae011bd6b86fa85b17b22f9227327e7ec6e68
AS1 <arseniykofanov@gmail.com>
2023-03-01 19:19:11 +0300

    Команда для перехода в меню таверны, а так же система выхода

--- bot/handlers/general.py
+++ bot/handlers/general.py
@@ -2,18 +2,31 @@ from telebot.types import Message
 
 from bot.config import mongo_client
 from bot.exec import bot
-from bot.modules.localization import t
-from bot.modules.markup import back_menu, markups_menu as m
+from bot.modules.localization import get_data, t
+from bot.modules.markup import back_menu
+from bot.modules.markup import markups_menu as m
+from bot.modules.user import User
+from bot.modules.data_format import user_name
+from asyncio import sleep
 
 users = mongo_client.bot.users
+management = mongo_client.bot.management
 
 @bot.message_handler(text='buttons_name.back', is_authorized=True)
 async def back_buttom(message: Message):
     userid = message.from_user.id
     lang = message.from_user.language_code
     back_m = back_menu(userid)
-    text = t(f'back_text.{back_m}', lang)
 
+    user_dict = users.find_one(
+           {'userid': userid}, {'last_markup': 1}
+        )
+    
+    if user_dict:
+        if user_dict.get('last_markup') == 'dino_tavern_menu':
+            management.update_one({'_id': 'in_tavern' }, {'$pull': {'users': userid}})
+
+    text = t(f'back_text.{back_m}', lang)
     await bot.send_message(message.chat.id, text, 
                            reply_markup=m(userid, back_m, lang) )
 
@@ -63,3 +76,36 @@ async def actions_menu(message: Message):
 
     await bot.send_message(message.chat.id, t('menu_text.actions', lang), 
                            reply_markup=m(userid, 'actions_menu', lang))
+    
+@bot.message_handler(text='commands_name.dino-tavern_menu', is_authorized=True)
+async def tavern_menu(message: Message):
+    userid = message.from_user.id
+    lang = message.from_user.language_code
+    user = User(userid)
+    friends = user.get_friends()['friends']
+    text = ''
+
+    management.update_one({'_id': 'in_tavern'}, {'$push': {'users': userid}})
+    users_in_tavern = management.find_one({'_id': 'in_tavern'})['users'] #type: ignore
+    friends_in_tavern = set(friends) & set(users_in_tavern)
+
+    await bot.send_message(message.chat.id, t('menu_text.dino_tavern.info', lang), reply_markup=m(userid, 'dino_tavern_menu', lang))
+    msg = await bot.send_message(message.chat.id, t('menu_text.dino_tavern.friends', lang))
+    text = t('menu_text.dino_tavern.friends2', lang)
+
+    if len(friends_in_tavern):
+        text += '\n'
+        for friendid in friends_in_tavern:
+            friendChat = await bot.get_chat_member(friendid, friendid)
+            friend = friendChat.user
+            if friend:
+                text += f' ● {user_name(friend)}\n'
+                text_to_friend = t('menu_text.dino_tavern.went', 
+                                   friend.language_code, 
+                                   name=user_name(message.from_user))
+                await bot.send_message(friendid, text_to_friend)
+                await sleep(0.5)
+    else:
+        text += '❌'
+
+    await bot.edit_message_text(text=text, chat_id=userid, message_id=msg.message_id)
