247fe6078d861c0691cc8128531352d9abf7de33
AS1 <arseniykofanov@gmail.com>
2023-03-01 16:22:31 +0300

    Код для работы кнопки измненения динозавра

---
+++ bot/handlers/actions.py
@@ -0,0 +1,48 @@
+from telebot.types import Message, CallbackQuery
+
+from bot.config import mongo_client
+from bot.exec import bot
+from bot.modules.data_format import list_to_inline
+from bot.modules.localization import t
+from bot.modules.markup import markups_menu as m
+from bot.modules.user import User
+
+users = mongo_client.bot.users
+dinosaurs = mongo_client.bot.dinosaurs
+
+
+@bot.message_handler(textstart='commands_name.actions.dino_button')
+async def edit_dino_buttom(message: Message):
+    user_id = message.from_user.id
+    user = User(user_id)
+    dinos = user.get_dinos()
+    data_names = {}
+
+    for element in dinos:
+        txt = f'🦕 {element.name}' #type: ignore
+        data_names[txt] = f'edit_dino {element.alt_id}'
+    
+    inline = list_to_inline([data_names], 2)
+    await bot.send_message(user_id, 
+                           t('edit_dino_button.edit', message.from_user.language_code), 
+                           reply_markup=inline)
+
+@bot.callback_query_handler(callbackstart='edit_dino', func=lambda call: True)
+async def answer_edit(call: CallbackQuery):
+    user_id = call.from_user.id
+    lang = call.from_user.language_code
+    user = User(user_id)
+
+    message = call.message
+    data = call.data.split()[1]
+
+    await bot.delete_message(user_id, message.id)
+    dino = dinosaurs.find_one({'alt_id': data}, {'_id': 1, 'name': 1})
+    if dino:
+        user.update({'$set': {'settings.last_dino': dino['_id']}})
+        await bot.send_message(user_id, 
+                               t('edit_dino_button.susseful', lang, 
+                               name=dino['name']),
+                               reply_markup=m(user_id, 'actions_menu', lang, True)
+                           )
+
