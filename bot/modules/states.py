bb9938bbd7f0d038a190e1e8dfce40c6de4dbeb2
AS1 <arseniykofanov@gmail.com>
2023-03-01 20:25:42 +0300

    Состояние для настроек

--- bot/modules/states.py
+++ bot/modules/states.py
@@ -2,3 +2,6 @@ from telebot.asyncio_handler_backends import State, StatesGroup
 
 class DinoStates(StatesGroup):
     choose_dino = State()
+
+class SettingsStates(StatesGroup):
+    settings_choose = State()
