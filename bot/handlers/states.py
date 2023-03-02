34f86c033782b3666ee48c0032ec29dae1be38ff
AS1 <arseniykofanov@gmail.com>
2023-03-02 17:11:28 +0300

    Перенесён хендлер с обработкой состояния выбора динозавра

--- bot/handlers/states.py
+++ bot/handlers/states.py
@@ -1,8 +1,10 @@
-from telebot.asyncio_handler_backends import State, StatesGroup
+
 from telebot.types import Message
 
 from bot.exec import bot
+from bot.modules.logs import log
 from bot.modules.markup import markups_menu as m
+from bot.modules.states import DinoStates
 
 
 @bot.message_handler(text='buttons_name.cancel', state='*')
@@ -21,3 +23,26 @@ async def get_state(message: Message):
     """
     state = await bot.get_state(message.from_user.id, message.chat.id)
     await bot.send_message(message.chat.id, f'{state}')
+    async with bot.retrieve_data(message.from_user.id, 
+                                 message.chat.id) as data: 
+        log(data)
+
+@bot.message_handler(state=DinoStates.choose_dino, is_authorized=True)
+async def answer_dino(message: Message):
+    """Общая функция для выбора динозавра
+    """
+    userid = message.from_user.id
+
+    async with bot.retrieve_data(userid, message.chat.id) as data:
+        ret_data = data['data']
+        func = data['function']
+
+    await bot.delete_state(userid, message.chat.id)
+    await bot.reset_data(message.from_user.id,  message.chat.id)
+
+    if message.text in ret_data.keys():
+        await func(message, ret_data[message.text])
+    else:
+        await bot.send_message(message.chat.id, "❌", 
+                    reply_markup=m(message.from_user.id, 
+                    'last_menu', message.from_user.language_code))
