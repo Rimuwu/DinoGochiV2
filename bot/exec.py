e68f44008a0e5b70940847d65ce09a3a5766895d
AS1 <arseniykofanov@gmail.com>
2023-03-02 15:51:44 +0300

    666

--- bot/exec.py
+++ bot/exec.py
@@ -23,6 +23,6 @@ def run():
     log('У вас логи не логятся :/')
     log('Не вижу ошибок == нет ошибок!')
 
-    add_task(bot.infinity_polling, skip_pending=True, timeout=300)
+    add_task(bot.infinity_polling, skip_pending=True, timeout=666)
     log('Все готово! Взлетаем!', prefix='Start')
     run_taskmanager()
