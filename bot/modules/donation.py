d54a8e3690b0ddaf64e6f53758da2f454825bcec
AS1 <arseniykofanov@gmail.com>
2023-03-05 16:57:45 +0300

    Тут много изменений, в общем модуль доната доделан

--- bot/modules/donation.py
+++ bot/modules/donation.py
@@ -6,9 +6,11 @@ import aiohttp
 
 from bot.config import conf, mongo_client
 from bot.exec import bot
-from bot.modules.currency import get_products
+from bot.modules.currency import get_products, get_all_currency
 from bot.modules.logs import log
 from bot.modules.notifications import user_notification
+from bot.modules.item import AddItemToUser
+from bot.modules.user import award_premium
 
 users = mongo_client.bot.users
 
@@ -21,8 +23,8 @@ headers = {
 def save(data):
     """Сохраняет данные в json
     """
-    with open(directory, 'w') as file:
-        json.dump(data, file, sort_keys=True, indent=4)
+    with open(directory, 'w', encoding='utf-8') as file:
+        json.dump(data, file, sort_keys=True, indent=4, ensure_ascii=False)
 
 def OpenDonatData() -> dict:
     """Загружает данные обработанных донатов
@@ -33,7 +35,7 @@ def OpenDonatData() -> dict:
             processed_donations = json.load(f)
     except Exception as error:
         if not os.path.exists(directory):
-            with open(directory, 'w') as f:
+            with open(directory, 'w', encoding='utf-8') as f:
                 f.write('{}')
         else:
             log(prefix='OpenDonatData', message=f'Error: {error}', lvl=4)
@@ -48,7 +50,7 @@ async def get_donations() -> list:
             return data['data']
 
 def save_donation(userid, amount, status, product, 
-            issued_reward, time_data):
+            issued_reward, time_data, col):
     """
     id: {
         'userid': int, 
@@ -56,7 +58,8 @@ def save_donation(userid, amount, status, product,
         'status': str,
         'product': str | None,
         'issued_reward': bool,
-        'time': int
+        'time': int,
+        'col': int
     }
     """
     data = {
@@ -65,7 +68,8 @@ def save_donation(userid, amount, status, product,
         'status': status,
         'product': product,
         'issued_reward': issued_reward,
-        'time': time_data
+        'time': time_data,
+        'col': col
     }
     return data
 
@@ -80,14 +84,15 @@ async def send_donat_notification(userid:int, message_key:str, **kwargs):
 
     await user_notification(userid, f'donation', lang, add_way=message_key, **kwargs)
 
-async def give_reward(userid:int, product_key:str):
+async def give_reward(userid:int, product_key:str, col:int):
     products = get_products()
-    product = products['product_key']
+    product = products[product_key]
     
-    if product['type'] == 'kit':
-        ...
-    elif product['type'] == 'subscription':
-        ...
+    if product['type'] == 'subscription':
+        award_premium(userid, product['time'] * col)
+    
+    for item_id in product['items']:
+        AddItemToUser(userid, item_id)
 
     await send_donat_notification(userid, 'reward')
 
@@ -95,10 +100,11 @@ async def check_donations():
     processed_donations = OpenDonatData()
     all_donations = await get_donations()
     products = get_products()
-    delete_doc = []
 
     for donat in all_donations:
         donation_data = {}
+        product_col = "1"
+
         if str(donat['id']) not in processed_donations.keys():
             #Донат не был ранее обработан
             if int(time()) - donat['created_at_ts'] <= 3024000:
@@ -106,30 +112,40 @@ async def check_donations():
                     userid = int(donat['username'])
                     user = users.find_one({'userid': userid})
                     if user:
-                        product_key = donat['message'].split('#$#')[0]
+                        message_split = donat['message'].split('#$#')
+                        product_key = message_split[0]
                         if product_key in products:
                             product = products[product_key]
 
-                            if donat['currency'] in product['cost']:
-                                cost = product['cost'][donat['currency']]
-                                rub_cost = product['cost']['RUB']
+                            if product['type'] == 'subscription':
+                                if len(message_split) > 1:
+                                    col = message_split[1]
+                                    if col.isdigit():
+                                        if col in product['cost'].keys():
+                                            product_col = col
+
+                            if donat['currency'] in get_all_currency():
+                                cost = product['cost'][product_col][donat['currency']]
+                                rub_cost = product['cost'][product_col]['RUB']
 
                                 if donat['amount'] >= cost:
-                                    await give_reward(userid, product_key)
+                                    await give_reward(userid, product_key, int(product_col))
 
                                     donation_data = save_donation(
                                         donat['username'], 
                                         donat['amount'], 'done', 
-                                        product_key, True, donat['created_at_ts']
+                                        product_key, True, donat['created_at_ts'],
+                                        product_col
                                     )
 
                                 elif donat['amount_in_user_currency'] >= rub_cost:
-                                    await give_reward(userid, product_key)
+                                    await give_reward(userid, product_key, int(product_col))
 
                                     donation_data = save_donation(
                                         donat['username'], 
                                         donat['amount'], 'done', 
-                                        product_key, True, donat['created_at_ts']
+                                        product_key, True, donat['created_at_ts'],
+                                        product_col
                                     )
 
                                 else:
@@ -138,7 +154,8 @@ async def check_donations():
                                     donation_data = save_donation(
                                         donat['username'], 
                                         donat['amount'], error, 
-                                        product_key, False, donat['created_at_ts']
+                                        product_key, False, donat['created_at_ts'],
+                                        product_col
                                     )
 
                                     donation_data['difference'] = [
@@ -148,23 +165,26 @@ async def check_donations():
                                     
                                     await send_donat_notification(userid, error, 
                                                                 difference=donation_data['difference'][0],
-                                                                currency=donat['currency']
+                                                                currency=donat['currency'],
+                                                                col=product_col
                                                                 )
                             else:
                                 #Валюта доната не найдена в возможных
                                 error = 'currency_key_error'
                                 donation_data = save_donation(donat['username'], 
                                     donat['amount'], error, 
-                                    product_key, False, donat['created_at_ts']
+                                    product_key, False, donat['created_at_ts'],
+                                    product_col
                                     )
                                 donation_data['error_key'] = donat['currency']
                                 await send_donat_notification(userid, error, 
-                                                            currency_key=donat['currency'])
+                                                            currency=donat['currency'])
                         else:
                             #неправильно указан ключ товара
                             error = 'product_key_error'
                             donation_data = save_donation(donat['username'], 
-                                donat['amount'], error, None, False, donat['created_at_ts']
+                                donat['amount'], error, None, False, donat['created_at_ts'],
+                                product_col
                                 )
                             donation_data['error_key'] = product_key[:12]
                             await send_donat_notification(userid, error, 
@@ -174,13 +194,15 @@ async def check_donations():
                         #id юзера не найдено в базе
                         error = 'userid_not_in_base'
                         donation_data = save_donation(donat['username'], 
-                            donat['amount'], error, None, False, donat['created_at_ts']
+                            donat['amount'], error, None, False, donat['created_at_ts'],
+                            product_col
                             )
                 else:
                     #id юзера может быть только число
                     error = 'userid_error'
                     donation_data = save_donation(donat['username'], 
-                            donat['amount'], error, None, False, donat['created_at_ts']
+                            donat['amount'], error, None, False, donat['created_at_ts'],
+                            product_col
                             )
         else:
             if int(time()) - donat['created_at_ts'] >= 3024001:
