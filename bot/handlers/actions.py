9ec93a6e4240a3f308388a6e5dedde599d11363d
AS1 <arseniykofanov@gmail.com>
2023-03-06 20:29:38 +0300

    Некая работа над активностью сон

--- bot/handlers/actions.py
+++ bot/handlers/actions.py
@@ -6,6 +6,7 @@ from bot.modules.data_format import list_to_inline
 from bot.modules.localization import t
 from bot.modules.markup import markups_menu as m
 from bot.modules.user import User
+from bot.modules.dinosaur import check_accessory, downgrade_accessory, Dino, start_sleep
 
 users = mongo_client.bot.users
 dinosaurs = mongo_client.bot.dinosaurs
@@ -48,26 +49,43 @@ async def answer_edit(call: CallbackQuery):
 async def short_sleep():
     ...
 
-async def long_sleep():
-    ...
+async def long_sleep(dino: Dino, userid: int, lang: str):
+    start_sleep(dino._id, 'long')
 
-@bot.message_handler(textstart='commands_name.actions.put_to_bed')
-async def put_to_bed(message: Message):
-    user_id = message.from_user.id
-    lang = message.from_user.language_code
+    await bot.send_message(userid, 
+                           t('put_to_bed.sleep', lang),
+                           reply_markup=m(userid, 'last_menu', lang, True)
+                          )
 
-    user = User(user_id)
-    last_dino = user.get_last_dino()
+# @bot.message_handler(textstart='commands_name.actions.put_to_bed')
+# async def put_to_bed(message: Message):
+#     user_id = message.from_user.id
+#     lang = message.from_user.language_code
 
-    if last_dino:
-        if last_dino.status == 'pass':
-            ...
-        else:
-            await bot.send_message(user_id, t('put_to_bed.alredy_busy', lang),
-                reply_markup=m(user_id, 'last_menu', lang))
-    else:
-        await bot.send_message(user_id, t('edit_dino_button.notfouned', lang),
-                reply_markup=m(user_id, 'last_menu', lang))
+#     user = User(user_id)
+#     last_dino = user.get_last_dino()
+
+#     if last_dino:
+#         if last_dino.status == 'pass':
+#             if last_dino.stats['energy'] >= 90:
+#                 await bot.send_message(message.chat.id, 
+#                                        t('put_to_bed.dont_want', lang)
+#                                        )
+            
+#             else:
+#                 if check_accessory(last_dino, '16'):
+#                     await long_sleep(last_dino, user_id, lang)
+#                 else:
+
+
+                
+
+#         else:
+#             await bot.send_message(user_id, t('put_to_bed.alredy_busy', lang),
+#                 reply_markup=m(user_id, 'last_menu', lang))
+#     else:
+#         await bot.send_message(user_id, t('edit_dino_button.notfouned', lang),
+#                 reply_markup=m(user_id, 'last_menu', lang))
         
         
 
