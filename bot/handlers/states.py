ffb00661efabaec8e625e763bcabdd662377b189
AS1 <arseniykofanov@gmail.com>
2023-03-07 18:20:27 +0300

    Команда для отмены

--- bot/handlers/states.py
+++ bot/handlers/states.py
@@ -9,7 +9,7 @@ from bot.modules.localization import t, get_data
 
 
 @bot.message_handler(text='buttons_name.cancel', state='*')
-async def cancel(message: Message):
+async def cancel_m(message: Message):
     """Состояние отмены
     """
     await bot.send_message(message.chat.id, "❌", 
@@ -17,6 +17,17 @@ async def cancel(message: Message):
                            'last_menu', message.from_user.language_code))
     await bot.delete_state(message.from_user.id, message.chat.id)
     await bot.reset_data(message.from_user.id,  message.chat.id)
+
+@bot.message_handler(commands=['cancel'], state='*')
+async def cancel_c(message: Message):
+    """Состояние отмены
+    """
+    await bot.send_message(message.chat.id, "❌", 
+                           reply_markup=m(message.from_user.id, 
+                           'last_menu', message.from_user.language_code))
+    await bot.delete_state(message.from_user.id, message.chat.id)
+    await bot.reset_data(message.from_user.id,  message.chat.id)
+
 
 @bot.message_handler(commands=['state'])
 async def get_state(message: Message):
