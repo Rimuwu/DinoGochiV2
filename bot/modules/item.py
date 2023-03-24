7eb65e096c81d521e4eefce5756a285a30203cb7
AS1 <arseniykofanov@gmail.com>
2023-03-24 17:42:45 +0300

    Все имена перенесены в локализцию => новая система сбора имён. Фцнкция получения описания. Фикс is_standart. Фикс assert в AddItemToUser RemoveItemFromUser. get_case_content - функция получения текста с вероятностью выпадения из кейса. counts_items - подсчёт предметов. item_info - получение информации о предмете

--- bot/modules/item.py
+++ bot/modules/item.py
@@ -3,6 +3,8 @@
     >>> abilities - словарь с индивидуальными харрактеристиками предмета, прочность, использования и тд.
     >>> preabil - используется только для предмета создаваемого из базы, используется для создания нестандартного предмета.
 """
+from pprint import pprint
+
 from bot.config import mongo_client
 from bot.const import ITEMS
 from bot.modules.data_format import random_dict
@@ -17,25 +19,27 @@ def get_data(itemid: str) -> dict:
     """Получение данных из json"""
 
     # Проверяем еть ли предмет с таким ключём в items.json
-    if itemid in ITEMS['items'].keys():
-        return ITEMS['items'][itemid]
+    if itemid in ITEMS.keys():
+        return ITEMS[itemid]
     else:
         return {}
 
 def load_items_names() -> dict:
-    """Загружает все имена предметов и имена из локалищации в один словарь. 
-       Приоритетным является локализация
+    """Загружает все имена предметов из локалищации в один словарь. 
     """
     items_names = {}
-
     loc_items_names = get_all_locales('items_names')
-    for item_key, item in ITEMS['items'].items():
-        items_names[item_key] = item['name']
-
+    
+    for item_key in ITEMS:
+        if item_key not in items_names:
+            items_names[item_key] = {}
+        
         for loc_key in loc_items_names.keys():
-           loc_name = loc_items_names[loc_key].get(item_key)
-           if loc_name:
-               items_names[item_key][loc_key] = loc_name['name']
+            loc_name = loc_items_names[loc_key].get(item_key)
+            if loc_name:
+                items_names[item_key][loc_key] = loc_name
+            else:
+                items_names[item_key][loc_key] = f"{item_key}_name"
     return items_names
 
 def get_name(itemid: str, lang: str='en') -> str:
@@ -45,8 +49,18 @@ def get_name(itemid: str, lang: str='en') -> str:
     if itemid in items_names:
         if lang not in items_names[itemid]:
             lang = 'en'
-        name = items_names[itemid][lang]
+        name = items_names[itemid][lang]['name']
     return name
+
+def get_description(itemid: str, lang: str='en') -> str:
+    """Получение описания предмета"""
+    description = ''
+   
+    if itemid in items_names:
+        if lang not in items_names[itemid]:
+            lang = 'en'
+        description = items_names[itemid][lang].get('description', '')
+    return description
 
 def get_item_dict(itemid: str, preabil: dict = {}) -> dict:
     ''' Создание словаря, хранящийся в инвентаре пользователя.\n
@@ -103,17 +117,20 @@ def is_standart(item: dict) -> bool:
         return True
     else:
         if 'abilities' in item.keys():
-            if item['abilities'] == data['abilities']:
-                return True
+            if item['abilities']:
+                if item['abilities'] == data['abilities']:
+                    return True
+                else:
+                    return False
             else:
-                return False
+                return True
         else:
             return True
 
 def AddItemToUser(userid: int, itemid: str, count: int = 1, preabil: dict = {}):
     """Добавление предмета в инвентарь
     """
-    assert count <= 0, f'AddItemToUser, count == {count}'
+    assert count >= 0, f'AddItemToUser, count == {count}'
 
     item = get_item_dict(itemid, preabil)
     find_res = items.find_one({'owner_id': userid, 'items_data': item}, {'_id': 1})
@@ -137,7 +154,7 @@ def RemoveItemFromUser(userid: int, itemid: str,
        True - всё нормально, удалил
        False - предмета нет или количесвто слишком большое
     """
-    assert count <= 0, f'RemoveItemFromUser, count == {count}'
+    assert count >= 0, f'RemoveItemFromUser, count == {count}'
 
     item = get_item_dict(itemid, preabil)
     find_res = items.find_one({'owner_id': userid, 'items_data': item}, {'_id': 1, 'count': 1})
@@ -236,7 +253,56 @@ def sort_materials(not_sort_list: list, lang: str,
 
     return f"{separator} ".join(items_list)
 
+def get_case_content(content: list, lang: str, separator: str = ' |'):
+    items_list = []
+    
+    for item in content:
+        name = get_name(item['id'], lang)
+        percent = int((item['chance'][0] / item['chance'][1]) * 100)
+        if item['col']['type'] == 'random':
+            col = f"x({item['col']['min']} - {item['col']['max']})"
+        else:
+            col = f"x{item['col']['act']}"
+        
+        items_list.append(
+            f'{name} {col} {percent}%'
+        )
+    return f"{separator} ".join(items_list)
+
+def counts_items(id_list: list, lang: str, separator: str = ','):
+    """Считает предмете, полученные в формате строки, 
+       и преобразовывает в текс.
+
+    Args:
+        id_list (list): Список с предметами в формате строки
+            example: ["1", "12"]
+        lang (str): Язык
+        separator (str, optional): Символы, разделяющие элементы. Defaults to ','.
+
+    Returns:
+        str: Возвращает строку для вывода материалов крафта
+    """
+    dct, items_list = {}, []
+    for i in id_list: dct[i] = dct.get(i, 0) + 1
+    
+    for item, col in dct.items():
+        name = get_name(item, lang)
+        if col > 1: name += f" x{col}"
+        
+        items_list.append(name)
+
+    return f"{separator} ".join(items_list)
+
 def item_info(item: dict, lang: str):
+    """Собирает информацию и предмете, пригодную для чтения
+
+    Args:
+        item (dict): Сгенерированный словарь данных предмета
+        lang (str): Язык
+
+    Returns:
+        Str, Image
+    """
     standart = ['freezing', 'defrosting', 'dummy', 'material']
     image = None
     
@@ -246,19 +312,131 @@ def item_info(item: dict, lang: str):
     rank_item: str = data_item['rank']
     type_item: str = data_item['type']
     
+    if 'class' in data_item:
+        type_loc: str = data_item['class']
+    else:
+        type_loc: str = data_item['type']
+    
     loc_d = get_loc_data('item_info', lang)
     text = ''
+    dp_text = ''
+    
+    # Шапка и название
+    text += loc_d['static']['cap'] + '\n'
+    text += loc_d['static']['name'].format(name=item_name) + '\n'
+
+    # Ранг предмета
+    rank = loc_d['rank'][rank_item]
+    text += loc_d['static']['rank'].format(rank=rank) + '\n'
+
+    # Тип предмета
+    type_name = loc_d['type_info'][type_loc]['type_name']
+    text += loc_d['static']['type'].format(type=type_name) + '\n'
     
     if type_item in standart:
-        text += loc_d['static']['cap'] + '\n'
-        text += loc_d['static']['name'].format(name=item_name) + '\n'
+        dp_text += loc_d['type_info'][type_loc]['add_text']
         
-        rank = loc_d['rank'][rank_item]
-        text += loc_d['static']['rank'].format(rank=rank) + '\n'
+    elif type_item == 'eat':
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(act=data_item['act'])
         
-        type_name = loc_d['type_info'][type_item]['type_name']
-        text += loc_d['static']['type'].format(type=type_name) + '\n'
-        text += loc_d['type_info'][type_item]['add_text']
+    elif type_item in ['game_ac', 'sleep_ac', 'journey_ac', 'collecting_ac']:
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(
+                item_decription=get_description(item_id, lang))
+    
+    elif type_item == 'recipe':
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(
+                create=sort_materials(data_item['create'], lang),
+                materials=sort_materials(data_item['materials'], lang),
+                item_decription=get_description(item_id, lang))
+    
+    elif type_item == 'weapon':
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(
+                ammunition=counts_items(data_item['ammunition'], lang),
+                min=data_item['damage']['min'],
+                max=data_item['damage']['max'])
+    
+    elif type_item == 'ammunition':
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(
+                add_damage=data_item['add_damage'])
+    
+    elif type_item == 'armor':
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(
+                reflection=data_item['reflection'])
+    
+    elif type_item == 'backpack':
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(
+                capacity=data_item['capacity'])
+    
+    elif type_item == 'case':
+        dp_text += loc_d['type_info'][
+            type_loc]['add_text'].format(
+                content=get_case_content(data_item['drop_items'], lang))
+    
+    if 'abilities' in item.keys():
+        if 'uses' in item['abilities'].keys():
+            text += loc_d['static']['uses'].format(
+                uses=item['abilities']['uses']
+            ) + '\n'
+
+        if 'endurance' in item['abilities'].keys():
+            text += loc_d['static']['endurance'].format(
+                endurance=item['abilities']['endurance']
+            ) + '\n'
+
+        if 'mana' in item['abilities'].keys():
+            text += loc_d['static']['mana'].format(
+                mana=item['abilities']['mana']
+            ) + '\n'
+
+        if 'stack' in item['abilities'].keys():
+            text += loc_d['static']['stack'].format(
+                stack=item['abilities']['stack']
+            ) + '\n'
+
+    text += dp_text
+    
+    all_bonuses = ['+mood', '+energy', '+eat', '+hp']
+    item_bonus = list(set(all_bonuses) & set(data_item.keys()))
+    
+    if item_bonus:
+        text += loc_d['static']['add_bonus']
+        bon_lst = []
+        
+        for bonus in item_bonus:
+            bon_lst.append(loc_d['bonuses'][bonus].format(data_item[bonus]))
+
+        end = bon_lst[-1]
+        for i in bon_lst:
+            if i == end:
+                text += '*└* '
+            else:
+                text += '*├* '
+            text += i
+    
+    all_penalties = ['-mood', "-eat", '-energy', '-hp']
+    item_penalties = list(set(all_penalties) & set(data_item.keys()))
+    
+    if item_penalties:
+        text += loc_d['static']['add_penaltie']
+        pen_lst = []
+        
+        for penaltie in item_penalties:
+            pen_lst.append(loc_d['penalties'][penaltie].format(data_item[penaltie]))
+
+        end = pen_lst[-1]
+        for i in pen_lst:
+            if i == end:
+                text += '*└* '
+            else:
+                text += '*├* '
+            text += i
     
     if 'image' in data_item.keys():
         try:
