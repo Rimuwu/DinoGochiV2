cb9a142f73800e72084160dc4173a653655007de
AS1 <arseniykofanov@gmail.com>
2023-04-30 14:42:20 +0300

    Информация о пользователе

--- bot/handlers/profile_menu.py
+++ bot/handlers/profile_menu.py
@@ -4,5 +4,21 @@ from bot.config import mongo_client
 from bot.exec import bot
 from bot.modules.localization import t
 from bot.modules.markup import markups_menu as m
+from bot.modules.user import user_info
 
 users = mongo_client.bot.users
+
+@bot.message_handler(text='commands_name.profile.information', 
+                     is_authorized=True)
+async def infouser(message: Message):
+    userid = message.from_user.id
+    chatid = message.chat.id
+    lang = message.from_user.language_code
+    
+    photos = await bot.get_user_profile_photos(userid, limit=1)
+    photo_id = photos.photos[0][0].file_id #type: ignore
+    
+    text = user_info(message.from_user, lang)
+    # await bot.send_message(message.chat.id, text, parse_mode='Markdown')
+    
+    await bot.send_photo(chatid, photo_id, text, parse_mode='Markdown')
