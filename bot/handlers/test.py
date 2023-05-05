993c1cacaea2fc2e1ff7ea82b8c097ee742e6ee6
AS1 <arseniykofanov@gmail.com>
2023-05-05 15:10:10 +0300

    Тестовые команды

--- bot/handlers/test.py
+++ bot/handlers/test.py
@@ -2,8 +2,8 @@
 
 from telebot.types import Message
 from bot.exec import bot
-from bot.modules.item import get_name, get_item_dict, get_data, AddItemToUser
-from bot.config import mongo_client
+from bot.modules.item import get_name, get_item_dict, get_data, AddItemToUser, DowngradeItem
+from bot.config import mongo_client, conf
 from bot.modules.user import User, max_dino_col
 from bot.modules.donation import check_donations, get_donations
 from time import time
@@ -15,6 +15,8 @@ from bot.modules.markup import list_to_keyboard, count_markup
 from asyncio import sleep
 from bot.const import ITEMS
 from bot.modules.states_tools import ChooseStepState
+from bot.modules.data_format import seconds_to_str 
+from bot.modules.images import dino_game
 
 dinosaurs = mongo_client.bot.dinosaurs
 
@@ -34,11 +36,32 @@ async def test_command(message: Message):
     
     await ChooseStepState(func, user_id, user_id, 'ru', steps)
 
+@bot.message_handler(commands=['add_item', 'item_add'])
+async def command(message):
+    user = message.from_user
+    if user.id in conf.bot_devs:
+        msg_args = message.text.split()
+
+        if len(msg_args) < 4:
+            print('-347')
+
+        else:
+            ad_user = int(msg_args[1])
+            item_id = msg_args[2]
+            col = int(msg_args[3])
+            
+            print('user', ad_user, 'id:', item_id, 'col:', col)
+            res = AddItemToUser(ad_user, item_id, col)
+            print(res)
+    else:
+        print(user.id, 'not in devs')
 
 
-@bot.message_handler(commands=['add_8'])
-async def add(message: Message):
-    user_id = message.from_user.id
+@bot.message_handler(commands=['img_test'])
+async def img(message):
+    user = User( message.from_user.id)
     
-    m, res = AddItemToUser(user_id, "8", 20)
-    print(m, res)
+    dino = user.get_last_dino()
+    if dino:
+        img = dino_game(dino.data_id, dino.age.days)
+        await bot.send_photo(message.chat.id, img, '13')
