0933ea10861aeb137904a2216a0793cdf09d078e
AS1 <arseniykofanov@gmail.com>
2023-05-12 17:28:46 +0300

    -

--- bot/handlers/test.py
+++ bot/handlers/test.py
@@ -1,22 +1,28 @@
 # Тестовые команды
 
-from telebot.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultContact
-from bot.exec import bot
-from bot.modules.item import get_name, get_item_dict, get_data, AddItemToUser, DowngradeItem
-from bot.config import mongo_client, conf
-from bot.modules.user import User, max_dino_col
-from bot.modules.donation import check_donations, get_donations
-from time import time
-from pprint import pprint
-from bot.modules.currency import convert
-from bot.modules.notifications import user_notification
-from bot.modules.inventory_tools import inventory_pages
-from bot.modules.markup import list_to_keyboard, count_markup
 from asyncio import sleep
-from bot.const import ITEMS
+from pprint import pprint
+from time import time
+
+from telebot.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                           InlineQueryResultContact, Message)
+
+from bot.config import conf, mongo_client
+from bot.const import ITEMS, GAME_SETTINGS
+from bot.exec import bot
+from bot.modules.currency import convert
+from bot.modules.data_format import seconds_to_str
+from bot.modules.donation import check_donations, get_donations
+from bot.modules.images import dino_game, dino_collecting
+from bot.modules.inventory_tools import inventory_pages
+from bot.modules.item import (AddItemToUser, DowngradeItem, get_data,
+                              get_item_dict, get_name)
+from bot.modules.markup import count_markup, list_to_keyboard
+from bot.modules.notifications import user_notification
 from bot.modules.states_tools import ChooseStepState
-from bot.modules.data_format import seconds_to_str 
-from bot.modules.images import dino_game
+from bot.modules.user import User, max_dino_col
+
+from bot.handlers.dino_profile import transition
 
 dinosaurs = mongo_client.bot.dinosaurs
 
@@ -57,11 +63,50 @@ async def command(message):
         print(user.id, 'not in devs')
 
 
-# @bot.message_handler(commands=['img_test'])
-# async def img(message):
-#     user = User( message.from_user.id)
+@bot.message_handler(commands=['img_test'])
+async def img(message):
+    user = User( message.from_user.id)
     
-#     dino = user.get_last_dino()
-#     if dino:
-#         img = dino_game(dino.data_id, dino.age.days)
-#         await bot.send_photo(message.chat.id, img, '13')
+    dino = user.get_last_dino()
+    if dino:
+        
+        for i in range(100):
+            img = dino_collecting(dino.data_id, 'all')
+            # await bot.send_photo(message.chat.id, img, '13')
+        
+        print('end')
+        
+
+@bot.message_handler(commands=['stress'])
+async def stress(message):
+    user = User( message.from_user.id)
+    dino = user.get_last_dino()
+    
+    transmitted_data = {
+        'userid': user.userid,
+        'lang': 'ru'
+    }
+    
+    for i in range(100):
+        print(i)
+        try:
+            await transition(dino, transmitted_data)
+        except:
+            print('except ', i)
+    
+    
+# @bot.message_handler(commands=['names'])
+# async def names(message):
+    
+#     for key in GAME_SETTINGS['collecting_items']:
+#         print('\n', key, 'key')
+#         for col_key in GAME_SETTINGS['collecting_items'][key]:
+#             print('\n', col_key, end=' ')
+#             for i in GAME_SETTINGS['collecting_items'][key][col_key]:
+                
+#                 print(
+#                     get_name(i, 'ru'), i
+#                 , end=' ')
+            
+    
+#     print('\n43')
