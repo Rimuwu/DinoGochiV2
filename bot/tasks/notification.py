8d4ca1a1a2906ffa34f93614e4d1e6bd5c7f9efb
AS1 <arseniykofanov@gmail.com>
2023-05-25 13:34:39 +0300

    Дополнительные чеки для уведомлений

--- bot/tasks/notification.py
+++ bot/tasks/notification.py
@@ -1,7 +1,5 @@
 from bot.config import conf, mongo_client
-from bot.modules.notifications import (check_dino_notification, critical_line,
-                                       dino_notification,
-                                       dino_notification_delete)
+from bot.modules.notifications import notification_manager
 from bot.taskmanager import add_task
 
 dinosaurs = mongo_client.bot.dinosaurs
@@ -12,16 +10,9 @@ async def dino_notifications():
     for dino in dinos:
         dino_id = dino['_id']
         for stat in dino['stats']:
+            
             unit = dino['stats'][stat]
-
-            if critical_line[stat] >= unit:
-                if check_dino_notification(dino_id, f'need_{stat}', False):
-                    # Отправка уведомления
-                    await dino_notification(dino_id, f'need_{stat}', unit=unit)
-
-            elif unit >= critical_line[stat] + 20:
-                # Удаляем уведомление 
-                dino_notification_delete(dino_id, f'need_{stat}')
+            await notification_manager(dino_id, stat, unit)
 
 if __name__ != '__main__':
     if conf.active_tasks:
