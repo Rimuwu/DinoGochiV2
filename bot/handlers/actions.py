126a6a7006b00b9dbd9d832f9fdaf0e34cb8b740
AS1 <arseniykofanov@gmail.com>
2023-06-05 21:17:27 +0300

    Работа над путеествиями

--- bot/handlers/actions.py
+++ bot/handlers/actions.py
@@ -556,3 +556,85 @@ async def collecting_callback(callback: CallbackQuery):
             await end_collecting(dino._id, 
                                  data['items'], data['sended'], 
                                  items_names)
+
+async def journey_start_adp(return_data: int, transmitted_data: dict):
+    userid = transmitted_data['userid']
+    lang = transmitted_data['lang']
+    chatid = transmitted_data['chatid']
+    dino = transmitted_data['last_dino']
+    
+    print(return_data)
+
+
+@bot.message_handler(text='commands_name.actions.journey')
+async def journey_start(message: Message):
+    userid = message.from_user.id
+    lang = message.from_user.language_code
+    chatid = message.chat.id
+    
+    user = User(userid)
+    last_dino = user.get_last_dino()
+    
+    premium_loc = ['magic-forest']
+
+    img = open('images/actions/journey/preview.png', 'rb')
+    content_data = get_data('journey_start', lang)
+    
+    text, a = content_data['ask_loc'], 1
+    buttons = {}
+
+    for key, dct in content_data['locations'].items():
+        text += f"*{a}*. {dct['text']}\n\n"
+        if user.premium or key not in premium_loc:
+            buttons[dct['name']] = f'j_location {key}'
+        a += 1
+
+    text_complexity, buttons_complexity = '', []
+    comp = content_data['complexity']
+    text_complexity = comp['text']
+    buttons_complexity.append({comp['button']: 'journey_complexity'})
+    
+    m1_reply = list_to_inline(buttons_complexity)
+    m2_reply = list_to_inline([buttons], 2)
+    
+    await bot.send_message(chatid, text_complexity, 
+                           reply_markup=m1_reply)
+    
+    # await bot.send_photo(chatid, img, text, 
+    #                      reply_markup=m2_reply, parse_mode='Markdown')
+    
+    text_time = content_data['time_info']
+    buttons_time = [{}, {}, {}]
+    b = 2
+    for key, dct in content_data['time_text'].items():
+        if len(buttons_time[2 - b].keys()) >= b + 1: b -= 1
+        buttons_time[2 - b][dct['text']] = f'j_time {key}'
+
+    m3_reply = list_to_inline(buttons_time)
+    # await bot.send_message(chatid, text_time, reply_markup=m3_reply)
+    
+    steps = [
+        {"type": 'inline', "name": 'location', 
+         "data": {"auth_key": 'j_location'}, 
+         "edit_message": True,
+         "image": 'images/actions/journey/preview.png',
+         "message": {"caption": text, "reply_markup": m2_reply, 
+                     }
+        },
+        {"type": 'inline', "name": 'time_key', 
+         "data": {"auth_key": 'j_time'}, 
+         "message": {"caption": text_time, "reply_markup": m3_reply}
+        }
+    ]
+    
+    await ChooseStepState(journey_start_adp, userid, chatid, lang, steps, {'last_dino': last_dino})
+
+
+@bot.callback_query_handler(func=
+                            lambda call: call.data.startswith('journey_complexity'))
+async def journey_complexity(callback: CallbackQuery):
+    lang = callback.from_user.language_code
+    chatid = callback.message.chat.id
+    
+    text = t('journey_complexity', lang)
+    await bot.send_message(chatid, text, parse_mode='Markdown')
