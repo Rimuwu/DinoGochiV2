62176242efbff420bfc4676928dc94e7ba3d108d
AS1 <arseniykofanov@gmail.com>
2023-06-06 21:49:39 +0300

    Работа над совместной активностью

--- bot/modules/friends.py
+++ bot/modules/friends.py
@@ -1,5 +1,12 @@
+
+
 from bot.config import mongo_client
+from bot.exec import bot
+from bot.modules.data_format import list_to_inline, user_name
+from bot.modules.localization import get_data, t
+
 friends = mongo_client.connections.friends
+game_task = mongo_client.tasks.game
 
 def get_frineds(userid: int) -> dict:
     """ Получает друзей (id) и запросы к пользователю
@@ -55,3 +62,36 @@ def insert_friend_connect(userid: int, friendid: int, action: str):
         }
         return friends.insert_one(data)
     return False
+
+async def send_action_invite(userid: int, friendid: int, action: str, 
+                             dino_alt: str, lang: str):
+    """ userid - отправитель
+        friendid - тот кто присоединится к активности
+    """
+    chat_user = None
+
+    try:
+        chat_user = await bot.get_chat_member(userid, userid)
+        username = user_name(chat_user.user)
+    except: username = '-'
+    
+    try:
+        chat2_user = await bot.get_chat_member(friendid, friendid)
+        friend_lang = chat2_user.user.language_code
+    except: friend_lang = lang
+
+    send_text = t(f'send_action.{action}.send', friend_lang, 
+                  username=username)
+    button = t(f'send_action.{action}.send_button', friend_lang)
+    markup = list_to_inline([{button: f'join_to_action {action} {dino_alt}'}])
+    try:
+        await bot.send_message(friendid, send_text, reply_markup=markup)
+        ok = True
+    except: ok = False
+
+    if chat_user and ok:
+        for_me = t(f'send_action.{action}.for_me', lang, 
+                   friendname=user_name(chat_user.user))
+        await bot.send_message(userid, for_me)
+    else:
+        await bot.send_message(userid, t('send_action.error', lang))
