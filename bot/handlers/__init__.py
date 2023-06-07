208d5ffeb825c6e801a73dee52b31aaa37f06ae4
AS1 <arseniykofanov@gmail.com>
2023-06-07 21:36:27 +0300

    -

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,10 +1,9 @@
 from . import states
-from . import mongoinfo
+
 from . import start
 from . import dino_profile
 from . import test
 from . import settings
-from . import actions
 from . import transition
 from . import inventory
 from . import about_menu
