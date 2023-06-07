44c1485c15cdcb148181bf8e1e2d165062baf7f1
AS1 <arseniykofanov@gmail.com>
2023-06-07 21:36:55 +0300

    -

--- bot/handlers/mongoinfo.py
+++
@@ -1,9 +0,0 @@
-from bot import config
-
-from telebot import types
-from bot.exec import bot
-
-@bot.message_handler(commands=['mongo_info'], is_admin=True)
-async def mongo_command(message: types.Message):
-    await bot.send_message(message.chat.id, str(config.mongo_client.server_info()))
-
