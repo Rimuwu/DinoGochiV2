11e3779cb1bc2da3884bf3bc8a8f42fd92604d0f
AS1 <arseniykofanov@gmail.com>
2023-06-08 15:28:06 +0300

    Хендлер удаления сообщения

--- bot/handlers/post_handler.py
+++ bot/handlers/post_handler.py
@@ -4,6 +4,13 @@ from telebot import types
 from bot.exec import bot
 from bot.modules.logs import log
 
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('delete_message'))
+async def delete_message(call: types.CallbackQuery):
+    chatid = call.message.chat.id
+    await bot.delete_message(chatid, call.message.id)
+    await bot.answer_callback_query(call.id, "🗑")
+
 @bot.callback_query_handler(func=lambda call: True)
 async def not_found(call: types.CallbackQuery):
     userid = call.from_user.id
