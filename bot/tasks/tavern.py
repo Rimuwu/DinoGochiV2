7a89905b9d66d8353aa4dacffe3b6bf680f2dffb
AS1 <arseniykofanov@gmail.com>
2023-06-13 21:16:45 +0300

    Таски таверны

--- bot/tasks/tavern.py
+++ bot/tasks/tavern.py
@@ -1,23 +1,43 @@
+from datetime import datetime, timezone
+from random import choice, randint
 from time import time
 
 from bot.config import conf, mongo_client
 from bot.exec import bot
 from bot.modules.data_format import seconds_to_str
+from bot.modules.localization import get_data, t
 from bot.modules.notifications import user_notification
+from bot.modules.quests import create_quest, quest_resampling, save_quest
 from bot.taskmanager import add_task
-from bot.modules.localization import t, get_data
-from random import randint, choice
-
 
 users = mongo_client.bot.users
 tavern = mongo_client.connections.tavern
+quests_data = mongo_client.connections.quests
 
 
-async def tavern_quest():
-    ...
+async def tavern_quest(user):
+    free_quests = list(quests_data.find({'owner_id': 0}, {'_id': 1}))
 
-async def tavern_replic():
-    ...
+    if free_quests and not randint(0, 3):
+        quest_id = choice(free_quests)['_id']
+        quests_data.update_one({'_id': quest_id})
+    else:
+        quest = create_quest()
+        save_quest(quest, user['userid'])
+    
+
+async def tavern_replic(in_tavern, user):
+    names = in_tavern.copy()
+    names.remove(user)
+
+    if names:
+        random_name = choice(names)['name']
+        random_replic = choice(get_data('tavern_dialogs', user['lang']))
+
+        text = f'👤 {random_name}: {random_replic}'
+        try:
+            await bot.send_message(user['userid'], text)
+        except Exception: pass
 
 async def tavern_life():
     in_tavern = list(tavern.find({}))
@@ -30,19 +50,25 @@ async def tavern_life():
             except Exception: pass
 
         elif randint(1, 10) == 5:
-            names = in_tavern.copy()
-            names.remove(user)
+            await tavern_replic(in_tavern, user)
 
-            if names:
-                random_name = choice(names)['name']
-                random_replic = choice(get_data('tavern_dialogs', user['lang']))
+        elif randint(1, 10) == 5:  await tavern_quest(user)
 
-                text = f'👤 {random_name}: {random_replic}'
-                try:
-                    await bot.send_message(user['userid'], text)
-                except Exception: pass
-        elif randint(1, 15) == 5: print('quest')
+async def quest_managment():
+    quests = quests_data.find({})
+    now = datetime.now(timezone.utc)
+
+    for quest in quests:
+        create = quest['_id'].generation_time
+        delta = now - create
+
+        if delta.secodnds >= 2592000:
+            quests_data.delete_one({'_id': quest['_id']})
+
+        elif int(time()) >= quest['time_end']:
+            quest_resampling(quest['_id'])
 
 if __name__ != '__main__':
     if conf.active_tasks:
         add_task(tavern_life, 180.0, 10.0) # 180.0, 10.0
+        add_task(tavern_life, 120.0, 10.0)
