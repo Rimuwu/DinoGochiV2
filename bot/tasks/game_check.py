6f8866490e9dcf9be432a8a61257007ec597eade
AS1 <arseniykofanov@gmail.com>
2023-06-22 18:57:53 +0300

    Фиксы

--- bot/tasks/game_check.py
+++ bot/tasks/game_check.py
@@ -27,23 +27,24 @@ async def game_end():
         await end_game(i['dino_id'])
         game_time = i['game_end'] - i['game_start']
         owner = get_owner(i['dino_id'])
-        quest_process(owner['owner_id'], 'game', (game_time) // 60)
+        if owner:
+            quest_process(owner['owner_id'], 'game', (game_time) // 60)
 
         add_mood(i['dino_id'], 'end_game', 1, 
                  int((game_time // 2) * i['game_percent']))
 
 async def game_process():
     data = list(game_task.find({'game_end': {'$gte': int(time())}})).copy()
-    
+
     for game_data in data:
         percent = game_data['game_percent']
         dino = dinosaurs.find_one({'_id': game_data['dino_id']})
-        
+
         if dino:
             if random.uniform(0, 1) <= ENERGY_DOWN:
                 if random.uniform(0, 1) <= ENERGY_DOWN2: 
                     await mutate_dino_stat(dino, 'energy', -1)
-    
+
             if dino['stats']['game'] < 100:
                 if random.uniform(0, 1) <= LVL_CHANCE: 
                     if not check_breakdown(dino['_id'], 'unrestrained_play'):
@@ -51,7 +52,7 @@ async def game_process():
                         if dino_con:
                             userid = dino_con['owner_id']
                             await experience_enhancement(userid, randint(1, 19))
-                
+
                 if dino['stats']['game'] < 100:
                     if random.uniform(0, 1) <= GAME_CHANCE:
                        await mutate_dino_stat(dino, 'game', int(randint(2, 10) * percent))
