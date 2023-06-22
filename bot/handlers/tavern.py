83cdb9074b279231d7ca932717ba72874866ddc0
AS1 <arseniykofanov@gmail.com>
2023-06-22 19:09:20 +0300

    Ежедневная награда

--- bot/handlers/tavern.py
+++ bot/handlers/tavern.py
@@ -1,22 +1,26 @@
-from telebot.types import CallbackQuery, Message, InlineKeyboardButton, InlineKeyboardMarkup
+from datetime import datetime, timedelta
+from time import time
 
-from bot.config import mongo_client, conf
+from telebot.types import (CallbackQuery, InlineKeyboardButton,
+                           InlineKeyboardMarkup, Message)
+
+from bot.config import conf, mongo_client
+from bot.const import GAME_SETTINGS as GS
 from bot.exec import bot
 from bot.modules.data_format import list_to_inline, seconds_to_str
+from bot.modules.dinosaur import Dino, create_dino_connection
 from bot.modules.friend_tools import start_friend_menu
 from bot.modules.friends import get_frineds, insert_friend_connect
+from bot.modules.item import counts_items
 from bot.modules.localization import get_data, t
 from bot.modules.markup import cancel_markup, confirm_markup, count_markup
 from bot.modules.markup import markups_menu as m
 from bot.modules.notifications import user_notification
 from bot.modules.states_tools import (ChooseConfirmState, ChooseCustomState,
-                                      ChoosePagesState, ChooseDinoState, ChooseStepState, ChooseIntState)
-from bot.modules.user import user_name, take_coins, user_in_chat, check_name, daily_award_con, AddItemToUser
-from bot.modules.dinosaur import Dino, create_dino_connection
-from bot.const import GAME_SETTINGS as GS
-from bot.modules.item import counts_items
-from datetime import datetime, timedelta
-from time import time
+                                      ChooseDinoState, ChooseIntState,
+                                      ChoosePagesState, ChooseStepState)
+from bot.modules.user import (AddItemToUser, check_name, daily_award_con,
+                              take_coins, user_in_chat, user_name)
 
 events = mongo_client.tasks.events
 
@@ -39,13 +43,10 @@ async def events_c(message: Message):
         text += f'{a}. {event_text}\n\n'
 
     await bot.send_message(chatid, text)
+    
 
-@bot.message_handler(text='commands_name.dino_tavern.daily_award', is_authorized=True)
-async def bonus(message: Message):
+async def bonus_message(user, message, lang):
     userid = message.from_user.id
-    lang = message.from_user.language_code
-    chatid = message.chat.id
-    user = message.from_user
 
     add_text = ''
     award_data = GS['daily_award']
@@ -91,6 +92,20 @@ async def bonus(message: Message):
     await bot.send_photo(message.chat.id, photo, 
             text, parse_mode='Markdown', reply_markup=markup_inline)
 
+@bot.message_handler(text='commands_name.dino_tavern.daily_award', is_authorized=True)
+async def bonus(message: Message):
+    lang = message.from_user.language_code
+    user = message.from_user
+    await bonus_message(user, message, lang)
+
+@bot.callback_query_handler(func=lambda call: 
+    call.data == 'daily_message', is_authorized=True)
+async def daily_message(callback: CallbackQuery):
+    user = callback.from_user
+    lang = user.language_code
+    message = callback.message
+    await bonus_message(user, message, lang)
+
 @bot.callback_query_handler(func=lambda call: call.data == 'daily_award', is_authorized=True)
 async def daily_award(callback: CallbackQuery):
     chatid = callback.message.chat.id
@@ -100,6 +115,7 @@ async def daily_award(callback: CallbackQuery):
     if sec := daily_award_con(userid):
         award_data = GS['daily_award']
         res = await user_in_chat(userid, conf.bot_group_id)
+
         key, add_bonus = 'lvl1', check_name(callback.from_user)
         if res: key = 'lvl2'
 
