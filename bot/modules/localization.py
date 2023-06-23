ac63a801255cc966bf35e2fc99199697f4423d31
AS1 <arseniykofanov@gmail.com>
2023-06-24 01:20:52 +0300

    -

--- bot/modules/localization.py
+++ bot/modules/localization.py
@@ -25,7 +25,7 @@ def load() -> None:
 def alternative_language(lang: str):
     languages = {
         'ua': 'ru',
-        # 'ru': 'en'
+        'ru': 'en'
     }
     if lang in languages: return languages[lang]
     return lang
