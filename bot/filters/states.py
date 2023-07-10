43ac14e8691bc605408726814b2a474ffc621ffe
AS1 <arseniykofanov@gmail.com>
2023-07-10 12:22:51 +0300

    Фильтр проверки состояния в текстовых хендлерах

--- bot/filters/states.py
+++ bot/filters/states.py
@@ -15,4 +15,17 @@ class NothingState(AdvancedCustomFilter):
         elif state and status: return False
         elif state and not status: return True
 
+class NothingState_str(AdvancedCustomFilter):
+    key = 'nothing_state_str'
+
+    async def check(self, message: Message, status: bool):
+        state = await bot.get_state(message.from_user.id, message.chat.id)
+
+        if not state and status: return True
+        elif not state and not status: return False
+
+        elif state and status: return False
+        elif state and not status: return True
+
 bot.add_custom_filter(NothingState())
+bot.add_custom_filter(NothingState_str())
