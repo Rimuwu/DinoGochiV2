a1f86ec6e33516d124a19e742edebe9bb35d7dd3
AS1 <arseniykofanov@gmail.com>
2023-07-19 14:00:04 +0300

    -

--- bot/handlers/friends.py
+++ bot/handlers/friends.py
@@ -3,7 +3,7 @@ from telebot.types import CallbackQuery, Message
 from bot.config import mongo_client
 from bot.exec import bot
 from bot.modules.data_format import list_to_inline
-from bot.modules.friend_tools import start_friend_menu
+from bot.modules.states_tools import start_friend_menu
 from bot.modules.friends import get_frineds, insert_friend_connect
 from bot.modules.localization import get_data, t, get_lang
 from bot.modules.markup import cancel_markup, confirm_markup, count_markup
