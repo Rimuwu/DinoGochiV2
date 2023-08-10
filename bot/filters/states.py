a0b425ab3481c435f8ee731d00ba2e931d730a5a
AS1 <arseniykofanov@gmail.com>
2023-08-10 13:40:30 +0300

    1 фильтр для всех обработчиков

--- bot/filters/states.py
+++ bot/filters/states.py
@@ -6,20 +6,11 @@ from bot.exec import bot
 class NothingState(AdvancedCustomFilter):
     key = 'nothing_state'
 
-    async def check(self, call: CallbackQuery, status: bool):
-        state = await bot.get_state(call.from_user.id, call.message.chat.id)
-
-        if not state and status: return True
-        elif not state and not status: return False
-
-        elif state and status: return False
-        elif state and not status: return True
-
-class NothingState_str(AdvancedCustomFilter):
-    key = 'nothing_state_str'
-
-    async def check(self, message: Message, status: bool):
-        state = await bot.get_state(message.from_user.id, message.chat.id)
+    async def check(self, var, status: bool):
+        if type(var) == CallbackQuery:
+            state = await bot.get_state(var.from_user.id, var.message.chat.id)
+        else: # Message
+            state = await bot.get_state(var.from_user.id, var.chat.id)
 
         if not state and status: return True
         elif not state and not status: return False
@@ -28,4 +19,3 @@ class NothingState_str(AdvancedCustomFilter):
         elif state and not status: return True
 
 bot.add_custom_filter(NothingState())
-bot.add_custom_filter(NothingState_str())
