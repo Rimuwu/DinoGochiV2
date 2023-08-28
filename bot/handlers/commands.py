b2ff2f3bee5bc19a10065862025fd3a4f3a1912e
AS1 <arseniykofanov@gmail.com>
2023-08-28 22:08:59 +0300

    команды

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -1,7 +1,12 @@
 from telebot.types import Message
+
 from bot.exec import bot
-from bot.modules.data_format import seconds_to_str
+from bot.config import mongo_client
+from bot.modules.data_format import seconds_to_str, user_name, str_to_seconds
+from bot.modules.inline import inline_menu
 from bot.modules.localization import get_lang, t
+
+
 
 @bot.message_handler(commands=['timer'])
 async def timer(message: Message):
@@ -20,6 +25,19 @@ async def timer(message: Message):
         except: text = 'error'
         await bot.send_message(chatid, text)
 
+@bot.message_handler(commands=['string_to_sec'], private=True)
+async def string_time(message):
+    txt = message.text.replace('/string_to_sec', '')
+    chatid = message.chat.id
+    lang = get_lang(message.from_user.id)
+
+    if txt == '':
+        text = t('string_to_str.info', lang)
+        await bot.send_message(chatid, text, parse_mode='Markdown')
+    else:
+        sec = str_to_seconds(txt)
+        await bot.send_message(chatid, str(sec))
+
 @bot.message_handler(commands=['push_info'])
 async def push_info(message: Message):
     chatid = message.chat.id
@@ -27,3 +45,14 @@ async def push_info(message: Message):
 
     text = t('push.push_info', lang)
     await bot.send_message(chatid, text, parse_mode='Markdown')
+
+@bot.message_handler(commands=['add_me'], private=False)
+async def profile(message: Message):
+    userid = message.from_user.id
+    lang = get_lang(message.from_user.id)
+
+    name = user_name(message.from_user, False)
+    text = t("add_me", lang, userid=userid, username=name)
+    await bot.reply_to(message, text, parse_mode='HTML',
+                    reply_markup=inline_menu('send_request', lang, userid=userid)
+                    )
