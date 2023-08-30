cfebf8ded5ca8990cbfc4d52d80dd32c5fc4859a
AS1 <arseniykofanov@gmail.com>
2023-08-30 22:09:40 +0300

    Промо

--- bot/modules/promo.py
+++ bot/modules/promo.py
@@ -5,7 +5,7 @@ from bot.exec import bot
 from bot.modules.data_format import (list_to_inline, list_to_keyboard,
                                      seconds_to_str, user_name, escape_markdown)
 from bot.modules.item import (AddItemToUser, CheckCountItemFromUser,
-                              RemoveItemFromUser, counts_items, get_name)
+                              RemoveItemFromUser, counts_items, get_name, items)
 from bot.modules.localization import get_data, get_lang, t
 from bot.modules.market import (add_product, preview_product,
                                 generate_items_pages, generate_sell_pages,
@@ -18,13 +18,10 @@ from bot.modules.markup import markups_menu as m
 from bot.modules.user import take_coins
 from random import choice
 from bot.modules.market import generate_items_pages
+from time import time
 
+promo = mongo_client.other.promo
 users = mongo_client.user.users
-management = mongo_client.other.management
-tavern = mongo_client.tavern.tavern
-sellers = mongo_client.market.sellers
-products = mongo_client.market.products
-
 
 async def create_promo_start(userid: int, chatid: int, lang: str):
 
@@ -176,4 +173,135 @@ async def end(return_data, transmitted_data):
     lang = transmitted_data['lang']
     userid = transmitted_data['userid']
     chatid = transmitted_data['chatid']
-    print(return_data)
+
+    code = transmitted_data['code']
+    coins = transmitted_data['coins']
+    count = transmitted_data['count']
+    time_end = transmitted_data['time_end']
+
+    if type(return_data['items']) != list:
+        add_items = [return_data['items']] * return_data['col']
+    else:     
+        add_items, a = [], 0
+        for item in return_data['items']:
+            add_items += [item] * return_data['col'][a]
+            a += 1
+
+    if time_end == 0: time_end = 'inf'
+
+    create_promo(code, count, time_end, coins, add_items)
+
+    text, markup = promo_ui(code, lang)
+    try:
+        await bot.send_message(chatid, text, parse_mode='Markdown', reply_markup=markup)
+    except:
+        await bot.send_message(chatid, text, reply_markup=markup)
+    await bot.send_message(chatid, '✅', reply_markup=m(userid, 'last_menu', lang))
+
+def create_promo(code: str, col: int, seconds, coins: int, items: list):
+    data = {
+        "code": code,
+        "users": [],
+        "col": col,
+        "time_end": seconds,
+        "time": seconds,
+        "coins": coins,
+        "items": items,
+        "active": False
+    }
+
+    promo.insert_one(data)
+
+def promo_ui(code: str, lang: str):
+    data = promo.find_one({"code": code})
+    text, markup = '', None
+
+    if data:
+        status = ''
+        if data['active']: status = '✅'
+        else: status = '❌'
+
+        id_list = []
+        for i in data['items']: id_list.append(i['item_id'])
+
+        if data['time_end'] == 'inf':
+            txt_time = '♾'
+        else: txt_time = seconds_to_str(data['time_end'], lang)
+
+        text = t('promo_commands.ui.text', lang,
+                 code=code, status=status,
+                 col=data['col'], coins=data['coins'],
+                 items=counts_items(id_list, lang),
+                 txt_time=txt_time)
+
+        but = get_data('promo_commands.ui.buttons', lang)
+        inl_l = {
+            but[0]: f'promo {code} activ',
+            but[1]: f'promo {code} delete',
+            but[2]: f'promo {code} use'
+        }
+
+        markup = list_to_inline([inl_l], 2)
+    return text, markup
+
+def get_promo_pages() -> dict:
+    res = list(promo.find({}))
+    data = {}
+    if res: 
+        for i in res: data[i['code']] = i['code']
+    return data
+
+def use_promo(code: str, userid: int, lang: str):
+    data = promo.find_one({"code": code})
+    user = users.find_one({'userid': userid}, {'userid': 1})
+    text = ''
+
+    if user:
+        if data:
+            col = data['col']
+            if col == 'inf': col = 1
+
+            end = promo['time_end']
+            if end == 'inf': end = int(time()) + 100
+
+            if data['active']:
+                if col:
+                    if end - int(time()) > 0:
+                        if userid not in data['users']:
+
+                            promo.update_one({"code": 'code'},
+                                             {"$push": {f'users': userid}
+                                              })
+
+                            if promo['col'] != 'inf':
+                                promo.update_one({"code": 'code'},
+                                                 {"$inc": {f'col': -1}})
+
+                            text = t('promo_commands.activate', lang)
+                            if data['coins']:
+                                text += t('promo_commands.coins', lang,
+                                          coins=data['coins']
+                                          )
+                            if data['items']:
+                                id_list = []
+                                for i in data['items']: id_list.append(i['item_id'])
+                                text += t('promo_commands.items', lang,
+                                          items=counts_items(id_list, lang)
+                                          )
+                        else:
+                            text = t('promo_commands.already_use', lang)
+                            return 'already_use', text
+                    else:
+                        text = t('promo_commands.time_end', lang)
+                        return 'time_end', text
+                else:
+                    text = t('promo_commands.max_col', lang)
+                    return 'max_col_use', text
+            else:
+                text = t('promo_commands.deactivated', lang)
+                return 'deactivated', text
+        else:
+            text = t('promo_commands.not_found', lang)
+            return 'not_found', text
+    text = t('promo_commands.no_user', lang)
+    return 'no_user', text
